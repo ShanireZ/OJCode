a8980a2fe259b04096d1d07cfe2d75495dac3284
ShanireZ <shanire@skylinejoy.com>
2021-08-01 09:50:19 +0800

    Daily

--- Luogu/P3372.cpp
+++ Luogu/P3372.cpp
@@ -1,110 +1,122 @@
-#include <iostream>
-#include <algorithm>
+#include <cstdio>
+#include <string>
 using namespace std;
+long long read()
+{
+    char ch = getchar();
+    while (ch != '-' && (ch > '9' || ch < '0'))
+    {
+        ch = getchar();
+    }
+    long long ans = 0, t = 1;
+    if (ch == '-')
+    {
+        t = -1;
+        ch = getchar();
+    }
+    while (ch >= '0' && ch <= '9')
+    {
+        ans = ans * 10 + (ch - '0');
+        ch = getchar();
+    }
+    return ans * t;
+}
+int n, m, pos, root;
+long long nums[100005];
 struct Node
 {
-	int l, r;
-	long long tot, lazy; //lazy表示当前节点值还未更新
+    int lc, rc;
+    long long tag, v;
 };
-Node ns[400005]; //线段树需4倍大小
-long long nums[100005];
-long long all, k, type;
-void sign(int id, int a, int b) //标记每个区间的范围 计算每个区间的和
+Node ns[400005];
+void pushdown(int now, int l, int r)
 {
-	ns[id].l = a, ns[id].r = b;
-	if (a != b)
-	{
-		int mid = (a + b) / 2;
-		sign(id * 2, a, mid);
-		sign(id * 2 + 1, mid + 1, b);
-		ns[id].tot = ns[id * 2].tot + ns[id * 2 + 1].tot;
-	}
-	else
-	{
-		ns[id].tot = nums[a];
-	}
+    if (ns[now].tag == 0)
+    {
+        return;
+    }
+    int mid = (l + r) / 2;
+    ns[ns[now].lc].v += (mid - l + 1) * ns[now].tag;
+    ns[ns[now].rc].v += (r - mid) * ns[now].tag;
+    ns[ns[now].lc].tag += ns[now].tag, ns[ns[now].rc].tag += ns[now].tag;
+    ns[now].tag = 0;
 }
-void pushdown(int id) //向下传递lazy 并更新节点tot
+void update(int now)
 {
-	if (ns[id].lazy)
-	{
-		long long lazy = ns[id].lazy;
-		ns[id].tot += lazy * (ns[id].r - ns[id].l + 1); //更新tot
-		if (ns[id].r != ns[id].l)						//如果存在子树
-		{
-			ns[id * 2].lazy += lazy;
-			ns[id * 2 + 1].lazy += lazy;
-		}
-		ns[id].lazy = 0; //当前节点lazy状态清空
-	}
+    ns[now].v = ns[ns[now].lc].v + ns[ns[now].rc].v;
 }
-void make(int id, int a, int b)
+void makeTree(int &now, int l, int r) //! now当前节点编号 l节点左边界 r节点有边界
 {
-	pushdown(id);
-	if (ns[id].l == a && ns[id].r == b) //当前区域是目标区域
-	{
-		if (type == 1)
-		{
-			ns[id].tot += k * (b - a + 1);
-			if (a != b) //处理子树的lazy a!=b说明存在子树
-			{
-				ns[id * 2].lazy += k;
-				ns[id * 2 + 1].lazy += k;
-			}
-		}
-		else
-		{
-			all += ns[id].tot;
-		}
-	}
-	else //当前区域不是目标区域
-	{
-		int mid = (ns[id].l + ns[id].r) / 2; //当前区域中点
-		int st = 0, ed = 0;					 //存储重合区域的两端
-		//左子树l~mid 右子树mid+1~r
-		if (a <= mid && b >= ns[id].l) //左子树有重合
-		{
-			st = max(a, ns[id].l);
-			ed = min(b, mid);
-			make(id * 2, st, ed);
-		}
-		if (a <= ns[id].r && b >= mid + 1) //右子树有重合
-		{
-			st = max(a, mid + 1);
-			ed = min(b, ns[id].r);
-			make(id * 2 + 1, st, ed);
-		}
-		if (type == 1) //值有增减才更新tot
-		{
-			ns[id].tot = ns[id * 2].tot + ns[id * 2 + 1].tot;
-			ns[id].tot += (ns[id * 2].r - ns[id * 2].l + 1) * ns[id * 2].lazy;
-			ns[id].tot += (ns[id * 2 + 1].r - ns[id * 2 + 1].l + 1) * ns[id * 2 + 1].lazy;
-		}
-	}
+    now = ++pos;
+    if (l == r)
+    {
+        ns[now].v = nums[l];
+        return;
+    }
+    int mid = (l + r) / 2;
+    makeTree(ns[now].lc, l, mid);
+    makeTree(ns[now].rc, mid + 1, r);
+    update(now);
+}
+void edit(int now, int l, int r, int x, int y, int k)
+{
+    pushdown(now, l, r);
+    if (x <= l && y >= r)
+    {
+        ns[now].v += (r - l + 1) * k;
+        ns[now].tag += k;
+        return;
+    }
+    int mid = (l + r) / 2;
+    if (x <= mid && l <= y)
+    {
+        edit(ns[now].lc, l, mid, x, y, k);
+    }
+    if (y >= mid + 1 && x <= r)
+    {
+        edit(ns[now].rc, mid + 1, r, x, y, k);
+    }
+    update(now);
+}
+long long querry(int now, int l, int r, int x, int y)
+{
+    pushdown(now, l, r);
+    if (x <= l && y >= r)
+    {
+        return ns[now].v;
+    }
+    int mid = (l + r) / 2;
+    long long ans = 0;
+    if (x <= mid && l <= y)
+    {
+        ans += querry(ns[now].lc, l, mid, x, y);
+    }
+    if (y >= mid + 1 && x <= r)
+    {
+        ans += querry(ns[now].rc, mid + 1, r, x, y);
+    }
+    return ans;
 }
 int main()
 {
-	int n, m;
-	cin >> n >> m;
-	for (int i = 1; i <= n; i++) //读入
-	{
-		cin >> nums[i];
-	}
-	sign(1, 1, n);				 //标记
-	for (int i = 1; i <= m; i++) //处理
-	{
-		int x, y;
-		cin >> type >> x >> y;
-		if (type == 1) //修改
-		{
-			cin >> k;
-		}
-		all = 0;
-		make(1, x, y); //处理
-		if (type == 2) //查询
-		{
-			cout << all << endl;
-		}
-	}
-	return 0;
+    n = read(), m = read();
+    for (int i = 1; i <= n; i++)
+    {
+        nums[i] = read();
+    }
+    makeTree(root, 1, n);
+    for (int i = 1; i <= m; i++)
+    {
+        int opt = read(), x = read(), y = read();
+        if (opt == 1) // todo 修改
+        {
+            long long k = read();
+            edit(root, 1, n, x, y, k);
+        }
+        else // todo 查询
+        {
+            printf("%lld\n", querry(root, 1, n, x, y));
+        }
+    }
+    return 0;
 }
--- Luogu/P3373.cpp
+++ Luogu/P3373.cpp
@@ -1,121 +1,134 @@
-#include <iostream>
+#include <cstdio>
 #include <algorithm>
 using namespace std;
+long long read();
 struct Node
 {
-	int l, r;
-	long long tot, lazyp, lazym; //lazytag表示当前节点值还未更新
+    int lc, rc;
+    long long v, add, mul;
 };
-Node ns[400005]; //线段树需4倍大小
-long long nums[100005];
-long long all, k, type, p;
-void sign(int id, int a, int b) //标记每个区间的范围 计算每个区间的和
+Node ns[400005];
+int pos, root;
+long long a[100005], mod;
+void update(int now)
 {
-	ns[id].l = a, ns[id].r = b, ns[id].lazym = 1;
-	if (a != b)
-	{
-		int mid = (a + b) / 2;
-		sign(id * 2, a, mid);
-		sign(id * 2 + 1, mid + 1, b);
-		ns[id].tot = (ns[id * 2].tot + ns[id * 2 + 1].tot) % p;
-	}
-	else
-	{
-		ns[id].tot = nums[a];
-	}
+    int lc = ns[now].lc, rc = ns[now].rc;
+    ns[now].v = (ns[lc].v + ns[rc].v) % mod;
 }
-void pushdown(int id) //向下传递lazytag 并更新节点tot
+void pushdown(int now, int l, int r)
 {
-	long long plus = ns[id].lazyp, mul = ns[id].lazym;
-	ns[id].tot = (ns[id].tot * mul + plus * (ns[id].r - ns[id].l + 1)) % p;
-	ns[id].lazyp = 0, ns[id].lazym = 1;
-	//先乘后加 如果新增加则直接加 如果新增乘则将加的部分也乘一下
-	if (ns[id].l != ns[id].r) //向左右子节点传递lazytag
-	{
-		ns[id * 2].lazym = mul * ns[id * 2].lazym % p;
-		ns[id * 2 + 1].lazym = mul * ns[id * 2 + 1].lazym % p;
-		ns[id * 2].lazyp = (plus + (ns[id * 2].lazyp * mul)) % p;
-		ns[id * 2 + 1].lazyp = (plus + (ns[id * 2 + 1].lazyp * mul)) % p;
-	}
+    int lc = ns[now].lc, rc = ns[now].rc, mid = (l + r) / 2;
+    ns[lc].v = (ns[lc].v * ns[now].mul % mod + ns[now].add * (mid - l + 1) % mod) % mod;
+    ns[rc].v = (ns[rc].v * ns[now].mul % mod + ns[now].add * (r - mid) % mod) % mod;
+    ns[lc].add = (ns[lc].add * ns[now].mul % mod + ns[now].add) % mod;
+    ns[rc].add = (ns[rc].add * ns[now].mul % mod + ns[now].add) % mod;
+    ns[lc].mul = ns[lc].mul * ns[now].mul % mod;
+    ns[rc].mul = ns[rc].mul * ns[now].mul % mod;
+    ns[now].mul = 1, ns[now].add = 0;
 }
-void make(int id, int a, int b)
+void newTree(int &now, int l, int r)
 {
-	pushdown(id);
-	if (ns[id].l == a && ns[id].r == b) //当前区域是目标区域
-	{
-		if (type == 1)
-		{
-			ns[id].tot = k * ns[id].tot % p;
-			if (a != b)
-			{
-				ns[id * 2].lazym = k * ns[id * 2].lazym % p;
-				ns[id * 2 + 1].lazym = k * ns[id * 2 + 1].lazym % p;
-				ns[id * 2].lazyp = k * ns[id * 2].lazyp % p;
-				ns[id * 2 + 1].lazyp = k * ns[id * 2 + 1].lazyp % p;
-			}
-		}
-		else if (type == 2)
-		{
-			ns[id].tot = (ns[id].tot + k * (b - a + 1)) % p;
-			if (a != b) //处理子树的lazytag a!=b说明存在子树
-			{
-				ns[id * 2].lazyp = (k + ns[id * 2].lazyp) % p;
-				ns[id * 2 + 1].lazyp = (k + ns[id * 2 + 1].lazyp) % p;
-			}
-		}
-		else
-		{
-			all = (ns[id].tot + all) % p;
-		}
-	}
-	else //当前区域不是目标区域
-	{
-		int mid = (ns[id].l + ns[id].r) / 2; //当前区域中点
-		int st = 0, ed = 0;					 //存储重合区域的两端
-		//左子树l~mid 右子树mid+1~r
-		if (a <= mid && b >= ns[id].l) //左子树有重合
-		{
-			st = max(a, ns[id].l);
-			ed = min(b, mid);
-			make(id * 2, st, ed);
-		}
-		if (a <= ns[id].r && b >= mid + 1) //右子树有重合
-		{
-			st = max(a, mid + 1);
-			ed = min(b, ns[id].r);
-			make(id * 2 + 1, st, ed);
-		}
-		if (type != 3) //值有增减才更新tot
-		{
-			int tot1 = (ns[id * 2].tot * ns[id * 2].lazym + ns[id * 2].lazyp * (ns[id * 2].r - ns[id * 2].l + 1)) % p;
-			int tot2 = (ns[id * 2 + 1].tot * ns[id * 2 + 1].lazym + ns[id * 2 + 1].lazyp * (ns[id * 2 + 1].r - ns[id * 2 + 1].l + 1)) % p;
-			ns[id].tot = (tot1 + tot2) % p;
-		}
-	}
+    now = ++pos;
+    ns[now].mul = 1;
+    if (l == r)
+    {
+        ns[now].v = a[l];
+        return;
+    }
+    int mid = (l + r) / 2;
+    newTree(ns[now].lc, l, mid);
+    newTree(ns[now].rc, mid + 1, r);
+    update(now);
+}
+void edit(int now, int l, int r, int a, int b, long long k, int t)
+{
+    if (a <= l && r <= b)
+    {
+        if (t == 1)
+        {
+            ns[now].v = ns[now].v * k % mod;
+            ns[now].add = ns[now].add * k % mod;
+            ns[now].mul = ns[now].mul * k % mod;
+        }
+        else
+        {
+            ns[now].v = (ns[now].v + (r - l + 1) * k % mod) % mod;
+            ns[now].add = (ns[now].add + k) % mod;
+        }
+        return;
+    }
+    pushdown(now, l, r);
+    int mid = (l + r) / 2;
+    if (a <= mid)
+    {
+        edit(ns[now].lc, l, mid, a, b, k, t);
+    }
+    if (b > mid)
+    {
+        edit(ns[now].rc, mid + 1, r, a, b, k, t);
+    }
+    update(now);
+}
+long long query(int now, int l, int r, int a, int b)
+{
+    if (a <= l && r <= b)
+    {
+        return ns[now].v;
+    }
+    pushdown(now, l, r);
+    int mid = (l + r) / 2;
+    long long ans = 0;
+    if (a <= mid)
+    {
+        ans = (ans + query(ns[now].lc, l, mid, a, b)) % mod;
+    }
+    if (b > mid)
+    {
+        ans = (ans + query(ns[now].rc, mid + 1, r, a, b)) % mod;
+    }
+    return ans;
 }
 int main()
 {
-	int n, m;
-	cin >> n >> m >> p;
-	for (int i = 1; i <= n; i++) //读入
-	{
-		cin >> nums[i];
-	}
-	sign(1, 1, n);				 //标记
-	for (int i = 1; i <= m; i++) //处理
-	{
-		int x, y;
-		cin >> type >> x >> y;
-		if (type != 3) //修改
-		{
-			cin >> k;
-		}
-		all = 0;
-		make(1, x, y); //处理
-		if (type == 3) //查询
-		{
-			cout << all % p << endl;
-		}
-	}
-	return 0;
+    int n = read(), m = read();
+    mod = read();
+    for (int i = 1; i <= n; i++)
+    {
+        a[i] = read() % mod;
+    }
+    newTree(root, 1, n);
+    for (int i = 1; i <= m; i++)
+    {
+        int opt = read(), x = read(), y = read();
+        if (opt == 1 || opt == 2) //mul = 1 add = 2
+        {
+            long long k = read() % mod;
+            edit(root, 1, n, x, y, k, opt);
+        }
+        else //query
+        {
+            printf("%lld\n", query(root, 1, n, x, y));
+        }
+    }
+    return 0;
+}
+long long read()
+{
+    char ch = getchar();
+    while (ch != '-' && (ch > '9' || ch < '0'))
+    {
+        ch = getchar();
+    }
+    long long ans = 0, t = 1;
+    if (ch == '-')
+    {
+        t = -1;
+        ch = getchar();
+    }
+    while (ch >= '0' && ch <= '9')
+    {
+        ans = ans * 10 + ch - '0';
+        ch = getchar();
+    }
+    return ans * t;
 }
