8684a72d250d1ac6bbe9a7cc2eb0d9781e5b26d0
ShanireZ <shanire@skylinejoy.com>
2021-08-02 11:20:51 +0800

    Update P3375 KMP.cpp

--- Luogu/P3375 KMP.cpp
+++ Luogu/P3375 KMP.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 string s1, s2;
 int jp[30], d[1000005], sz1, sz2;
-void border()
+void border() //! 求D数组
 {
     for (int i = 1, j = 0; i < sz2; i++)
     {
@@ -23,15 +23,15 @@ void kmp()
     int i = 0, j = 0;
     while (i < sz1)
     {
-        while (i < sz1 && j < sz2 && s1[i] == s2[j])
+        while (i < sz1 && j < sz2 && s1[i] == s2[j]) //! 尝试匹配
         {
             i++, j++;
-            if (j == sz2)
+            if (j == sz2) //! P完全匹配输出起始位置
             {
                 printf("%d\n", i - j + 1);
             }
         }
-        (j == 0) ? i++ : j = d[j - 1];
+        (j == 0) ? i++ : j = d[j - 1]; //! 匹配失败跳出 或 匹配完毕跳出
     }
 }
 int main()
