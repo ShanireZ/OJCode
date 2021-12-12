9017ba19efa55edb674983c23b4e1e2b3623137a
ShanireZ <shanire@skylinejoy.com>
2021-12-12 10:47:31 +0800

    Daily

--- Luogu/P2709.cpp
+++ Luogu/P2709.cpp
@@ -1,77 +1,90 @@
-#include <iostream>
+#include <cstdio>
 #include <algorithm>
 #include <cmath>
+#include <cstring>
 using namespace std;
-int a[50005], p[50005], ts[50005], ans[50005];
+int read();
+int n, m, k, bklen, bksz;
+int nums[50005], ts[50005], ans[50005];
 struct Quest
 {
-	int l, r, id;
+    int l, r, id;
+    bool operator<(const Quest oth) const
+    {
+        if (l / bklen == oth.l / bklen)
+        {
+            return r < oth.r;
+        }
+        return l / bklen < oth.l / bklen;
+    }
 };
-Quest quest[50005];
-bool cmp(Quest a, Quest b)
+Quest qst[50005];
+void makeAns()
 {
-	if (p[a.l] == p[b.l])
-	{
-		return a.r < b.r;
-	}
-	return p[a.l] < p[b.l];
-}
-void add(int x, int p)
-{
-	int num = a[x];
-	ans[p] -= ts[num] * ts[num];
-	ts[num]++;
-	ans[p] += ts[num] * ts[num];
-}
-void sub(int x, int p)
-{
-	int num = a[x];
-	ans[p] -= ts[num] * ts[num];
-	ts[num]--;
-	ans[p] += ts[num] * ts[num];
+    int l = 1, r = 0, now = 0;
+    for (int i = 1; i <= m; i++)
+    {
+        while (r < qst[i].r)
+        {
+            r++;
+            now = now - ts[nums[r]] * ts[nums[r]] + (ts[nums[r]] + 1) * (ts[nums[r]] + 1);
+            ts[nums[r]]++;
+        }
+        while (r > qst[i].r)
+        {
+            now = now - ts[nums[r]] * ts[nums[r]] + (ts[nums[r]] - 1) * (ts[nums[r]] - 1);
+            ts[nums[r]]--;
+            r--;
+        }
+        while (l < qst[i].l)
+        {
+            now = now - ts[nums[l]] * ts[nums[l]] + (ts[nums[l]] - 1) * (ts[nums[l]] - 1);
+            ts[nums[l]]--;
+            l++;
+        }
+        while (l > qst[i].l)
+        {
+            l--;
+            now = now - ts[nums[l]] * ts[nums[l]] + (ts[nums[l]] + 1) * (ts[nums[l]] + 1);
+            ts[nums[l]]++;
+        }
+        ans[qst[i].id] = now;
+    }
 }
 int main()
 {
-	int n, m, k;
-	cin >> n >> m >> k;
-	int block = sqrt(n);
-	for (int i = 1; i <= n; i++)
-	{
-		cin >> a[i];
-		p[i] = i / block;
-	}
-	for (int i = 1; i <= m; i++)
-	{
-		cin >> quest[i].l >> quest[i].r;
-		quest[i].id = i;
-	}
-	sort(quest + 1, quest + 1 + m, cmp);
-	int st = 1, ed = 0;
-	for (int i = 1; i <= m; i++)
-	{
-		int now = quest[i].id;
-		int pre = quest[i - 1].id;
-		ans[now] = ans[pre];
-		while (quest[i].l < st)
-		{
-			add(--st, now);
-		}
-		while (quest[i].l > st)
-		{
-			sub(st++, now);
-		}
-		while (quest[i].r > ed)
-		{
-			add(++ed, now);
-		}
-		while (quest[i].r < ed)
-		{
-			sub(ed--, now);
-		}
-	}
-	for (int i = 1; i <= m; i++)
-	{
-		cout << ans[i] << endl;
-	}
-	return 0;
+    n = read(), m = read(), k = read();
+    bklen = sqrt(n);
+    bksz = n / bklen + 1;
+    for (int i = 1; i <= n; i++)
+    {
+        nums[i] = read();
+    }
+    for (int i = 1; i <= m; i++)
+    {
+        qst[i].l = read(), qst[i].r = read();
+        qst[i].id = i;
+    }
+    sort(qst + 1, qst + 1 + m);
+    makeAns();
+    for (int i = 1; i <= m; i++)
+    {
+        printf("%d\n", ans[i]);
+    }
+    return 0;
+}
+int read()
+{
+    int ans = 0;
+    char ch = getchar();
+    while (ch > '9' || ch < '0')
+    {
+        ch = getchar();
+    }
+    while (ch >= '0' && ch <= '9')
+    {
+        ans = ans * 10 + ch - '0';
+        ch = getchar();
+    }
+    return ans;
 }
