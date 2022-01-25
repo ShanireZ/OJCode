a9c9eb3f24097534daeec9e2ce681d9931f93881
ShanireZ <shanire@skylinejoy.com>
2022-01-25 14:59:39 +0800

    Daily

--- Luogu/P1596.cpp
+++ Luogu/P1596.cpp
@@ -1,45 +1,47 @@
 #include <iostream>
-#include <algorithm>
 using namespace std;
-int n, m;
-char farm[105][105];
+char mp[105][105];
 int vis[105][105];
-int move[8][2] = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
+int n, m, ans, f[10][2] = {{0, 1}, {1, 1}, {-1, 1}, {1, 0}, {-1, 0}, {0, -1}, {1, -1}, {-1, -1}};
 void dfs(int x, int y)
 {
 	vis[x][y] = 1;
-	for(int i = 0; i < 8; i++)
+	for (int i = 0; i < 8; i++)
 	{
-		int nx = x + move[i][0];
-		int ny = y + move[i][1];
-		if(farm[nx][ny] == 'W' && vis[nx][ny] == 0)
+		int nx = x + f[i][0], ny = y + f[i][1];
+		if (nx < 1 || ny < 1 || nx > n || ny > m)
 		{
-			dfs(nx, ny);
+			continue;
 		}
-	}	
+		if (vis[nx][ny] == 1 || mp[nx][ny] == '.')
+		{
+			continue;
+		}
+		dfs(nx, ny);
+	}
 }
 int main()
 {
 	cin >> n >> m;
-	for(int i = 1; i <= n; i++)
+	for (int i = 1; i <= n; i++)
 	{
-		for(int j = 1; j <= m; j++)
+		for (int j = 1; j <= m; j++)
 		{
-			cin >> farm[i][j];
+			cin >> mp[i][j];
 		}
 	}
-	int ans = 0;
-	for(int i = 1; i <= n; i++)
+	for (int i = 1; i <= n; i++)
 	{
-		for(int j = 1; j <= m; j++)
+		for (int j = 1; j <= m; j++)
 		{
-			if(farm[i][j] == 'W' && vis[i][j] == 0)
+			if (mp[i][j] == '.' || vis[i][j] == 1)
 			{
-				dfs(i, j);
-				ans++;
+				continue;
 			}
-		}	
+			dfs(i, j);
+			ans++;
+		}
 	}
-	cout << ans;	
+	cout << ans << endl;
 	return 0;
 }
