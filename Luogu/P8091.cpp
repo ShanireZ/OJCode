7910a883beb854a24308f408405dada52114e4de
ShanireZ <shanire@skylinejoy.com>
2022-02-19 11:49:04 +0800

    Daily

--- Luogu/P8091.cpp
+++ Luogu/P8091.cpp
@@ -1,5 +1,52 @@
 #include <iostream>
 using namespace std;
+int judge(int x[], int y[])
+{
+    int wx = 0, wy = 0;
+    for (int i = 1; i <= 4; i++)
+    {
+        for (int j = 1; j <= 4; j++)
+        {
+            if (x[i] > y[j])
+            {
+                wx++;
+            }
+            else if (x[i] < y[j])
+            {
+                wy++;
+            }
+        }
+    }
+    return wx - wy;
+}
+bool checkc(int a[], int b[])
+{
+    for (int p1 = 1; p1 <= 10; p1++)
+    {
+        for (int p2 = 1; p2 <= 10; p2++)
+        {
+            for (int p3 = 1; p3 <= 10; p3++)
+            {
+                for (int p4 = 1; p4 <= 10; p4++)
+                {
+                    int c[5] = {0, p1, p2, p3, p4};
+                    int x = judge(b, c);
+                    if (x <= 0)
+                    {
+                        continue;
+                    }
+                    x = judge(c, a);
+                    if (x <= 0)
+                    {
+                        continue;
+                    }
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
 int main()
 {
     int t;
@@ -15,86 +62,12 @@ int main()
         {
             cin >> b[i];
         }
-        int wa = 0, wb = 0, wc = 0;
-        for (int i = 1; i <= 4; i++)
-        {
-            for (int j = 1; j <= 4; j++)
-            {
-                if (a[i] > b[j])
-                {
-                    wa++;
-                }
-                else if (a[i] < b[j])
-                {
-                    wb++;
-                }
-            }
-        }
-        if (wb > wa) // 让a>b
+        int x = judge(a, b);
+        if (x < 0)
         {
             swap(a, b);
         }
-        else if (wa == wb)
-        {
-            cout << "no" << endl;
-            continue;
-        }
-        int trig = 1;
-        for (int p1 = 1; p1 <= 10; p1++)
-        {
-            for (int p2 = 1; p2 <= 10; p2++)
-            {
-                for (int p3 = 1; p3 <= 10; p3++)
-                {
-                    for (int p4 = 1; p4 <= 10; p4++)
-                    {
-                        int c[5] = {0, p1, p2, p3, p4};
-                        wb = wc = 0;
-                        for (int i = 1; i <= 4; i++)
-                        {
-                            for (int j = 1; j <= 4; j++)
-                            {
-                                if (b[i] > c[j])
-                                {
-                                    wb++;
-                                }
-                                else if (b[i] < c[j])
-                                {
-                                    wc++;
-                                }
-                            }
-                        }
-                        if (wc >= wb)
-                        {
-                            continue;
-                        }
-                        wa = wc = 0;
-                        for (int i = 1; i <= 4; i++)
-                        {
-                            for (int j = 1; j <= 4; j++)
-                            {
-                                if (c[i] > a[j])
-                                {
-                                    wc++;
-                                }
-                                else if (c[i] < a[j])
-                                {
-                                    wa++;
-                                }
-                            }
-                        }
-                        if (wa >= wc)
-                        {
-                            continue;
-                        }
-                        trig = 0;
-                        goto check;
-                    }
-                }
-            }
-        }
-    check:
-        if (trig == 1)
+        if (x == 0 || checkc(a, b) == 0)
         {
             cout << "no" << endl;
         }
