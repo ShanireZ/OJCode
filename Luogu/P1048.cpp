96708091e1a197c02e4d0ad50c83e6a3d2cf2be9
ShanireZ <shanire@skylinejoy.com>
2022-05-08 08:39:51 +0800

    Daily

--- Luogu/P1048.cpp
+++ Luogu/P1048.cpp
@@ -1,46 +1,19 @@
 #include <iostream>
-#include <algorithm>
-#include <cstring>
 using namespace std;
-struct Herb
-{
-    int v, t;
-};
-Herb herbs[105];      //01背包问题
-int check[105][1005]; //存储前a个草药采集时间不超过b时，最大的总价值
-int sum(int a, int b)
-{
-    if (check[a][b] == -1)
-    {
-        if (b < herbs[a].t)
-        {
-            check[a][b] = sum(a - 1, b);
-        }
-        else
-        {
-            check[a][b] = max(sum(a - 1, b), sum(a - 1, b - herbs[a].t) + herbs[a].v);
-        }
-    }
-    return check[a][b];
-}
-
+int dp[1005];
 int main()
 {
-    int time, type;
-    cin >> time >> type;
-    memset(check, -1, sizeof(check));
-    for (int i = 1; i <= type; i++)
+    int t, m;
+    cin >> t >> m;
+    for (int i = 1; i <= m; i++)
     {
-        cin >> herbs[i].t >> herbs[i].v;
+        int cost, v;
+        cin >> cost >> v;
+        for (int j = t; j >= cost; j--)
+        {
+            dp[j] = max(dp[j], v + dp[j - cost]);
+        }
     }
-    for (int i = 0; i <= time; i++)
-    {
-        check[0][i] = 0;
-    }
-    for (int i = 0; i <= type; i++)
-    {
-        check[i][0] = 0;
-    }
-    cout << sum(type, time);
+    cout << dp[t] << endl;
     return 0;
 }
