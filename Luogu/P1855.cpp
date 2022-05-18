6a76ac6be107198b986f1c79c021a10934ff73ad
ShanireZ <shanire@skylinejoy.com>
2022-05-18 09:58:33 +0800

    Daily

--- Luogu/P1855.cpp
+++ Luogu/P1855.cpp
@@ -1,33 +1,22 @@
 #include <iostream>
-#include <algorithm>
-#include <cstring>
 using namespace std;
-int cnt[105][2];
-int dp[205][205]; //前i个愿望时，不超过m元t时间，完成的最大愿望数量，已压缩空间所以需从后向前倒推
+int dp[205][205];
 int main()
 {
     int n, m, t;
     cin >> n >> m >> t;
     for (int i = 1; i <= n; i++)
     {
-        cin >> cnt[i][0] >> cnt[i][1];
-    }
-    for (int i = 1; i <= n; i++)
-    {
-        for (int j = m; j >= 1; j--)
+        int mi, ti;
+        cin >> mi >> ti;
+        for (int j = m; j >= mi; j--)
         {
-            if (j >= cnt[i][0])
+            for (int k = t; k >= ti; k--)
             {
-                for (int k = t; k >= 1; k--)
-                {
-                    if (k >= cnt[i][1])
-                    {
-                        dp[j][k] = max(dp[j][k], dp[j - cnt[i][0]][k - cnt[i][1]] + 1);
-                    }
-                }
+                dp[j][k] = max(dp[j][k], dp[j - mi][k - ti] + 1);
             }
         }
     }
-    cout << dp[m][t];
+    cout << dp[m][t] << endl;
     return 0;
 }
