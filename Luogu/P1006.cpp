d4075ba693adaef033ee50d8b53c837585bdbdfa
ShanireZ <shanire@skylinejoy.com>
2022-05-27 16:46:06 +0800

    Daily

--- Luogu/P1006.cpp
+++ Luogu/P1006.cpp
@@ -1,37 +1,34 @@
 #include <iostream>
-#include <algorithm>
-#include <cstring>
 using namespace std;
-int nums[55][55];
-int dp[105][55][55]; //第i步时，第一个纸条位于第j行，第二个位于第k行，经过的最大路径和
-int same(int a, int b)
-{
-    return a != b;
-}
-
+int mp[55][55], t[105][55][55];
 int main()
 {
-    int m, n;
-    cin >> m >> n;
-    for (int i = 1; i <= m; i++)
+    int n, m;
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++)
     {
-        for (int j = 1; j <= n; j++)
+        for (int j = 1; j <= m; j++)
         {
-            cin >> nums[i][j];
+            cin >> mp[i][j];
         }
     }
-    for (int i = 1; i <= m + n - 2; i++)
+    t[0][1][1] = mp[1][1];
+    for (int step = 1; step <= m + n - 2; step++)
     {
-        for (int j = 1; j <= i + 1; j++)
+        for (int a = max(1, step + 2 - m); a <= min(n, step + 1); a++)
         {
-            for (int k = 1; k <= i + 1; k++)
+            for (int b = max(1, step + 2 - m); b <= min(n, step + 1); b++)
             {
-                int ans1 = max(dp[i - 1][j][k], dp[i - 1][j - 1][k]); //第i步时，第一个纸条位于第j行，第二个位于第k行，那么第i-1步总共有4种可能
-                int ans2 = max(dp[i - 1][j][k - 1], dp[i - 1][j - 1][k - 1]);
-                dp[i][j][k] = max(ans1, ans2) + nums[j][2 + i - j] + nums[k][2 + i - k] * same(j, k); //如果第i步时两个在同一行，那么说明两个在同一个格子，只需加一遍
+                int ans1 = max(t[step - 1][a][b], t[step - 1][a - 1][b - 1]);
+                int ans2 = max(t[step - 1][a - 1][b], t[step - 1][a][b - 1]);
+                t[step][a][b] = max(ans1, ans2) + mp[a][step + 2 - a];
+                if (a != b)
+                {
+                    t[step][a][b] += mp[b][step + 2 - b];
+                }
             }
         }
     }
-    cout << dp[m + n - 2][m][m];
+    cout << t[m + n - 2][n][n] << endl;
     return 0;
 }
