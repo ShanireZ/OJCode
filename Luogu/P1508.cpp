737ec3f14e9b350145fa88edada4e330cad95836
ShanireZ <shanire@skylinejoy.com>
2022-05-27 14:27:34 +0800

    Daily

--- Luogu/P1508.cpp
+++ Luogu/P1508.cpp
@@ -1,45 +1,35 @@
-#include <iostream>
-#include <algorithm>
 #include <cstring>
+#include <iostream>
 using namespace std;
-int nums[205][205];
-int total[205][205];
+long long mp[205][205], tot[205][205];
 int main()
 {
-    int m, n;
-    cin >> m >> n;
-    for (int i = 1; i <= m; i++)
-    {
-        for (int j = 1; j <= n; j++)
-        {
-            cin >> nums[i][j];
-        }
-    }
-    memset(total, 128, sizeof(total)); //无法到达的取值负无穷
-    int start = n / 2 + 1;
-    int end = n / 2 + 1;
-    total[m + 1][start] = 0; //出发点
-    for (int i = m; i >= 1; i--)
-    {
-        start = max(1, start - 1); //当前行有取值范围
-        end = min(n, end + 1);
-        for (int j = start; j <= end; j++)
-        {
-            int left = max(1, j - 1);
-            int right = min(n, j + 1);
-            int ans = max(total[i + 1][left], total[i + 1][right]);
-            ans = max(ans, total[i + 1][j]);
-            total[i][j] = ans + nums[i][j];
-        }
-    }
-    int ans = -0x3f3f3f3f;
+    int n, m;
+    cin >> n >> m;
+    memset(mp, 128, sizeof(mp));
     for (int i = 1; i <= n; i++)
     {
-        if (total[1][i] > ans)
+        for (int j = 1; j <= m; j++)
         {
-            ans = total[1][i];
+            cin >> mp[i][j];
         }
     }
-    cout << ans;
+    for (int i = n + 1; i >= 1; i--)
+    {
+        int st = max(m / 2 - (n - i), 1);
+        int ed = min(m / 2 + 2 + (n - i), m);
+        for (int j = st; j <= ed; j++)
+        {
+            tot[i - 1][j - 1] = max(tot[i - 1][j - 1], mp[i - 1][j - 1] + tot[i][j]);
+            tot[i - 1][j] = max(tot[i - 1][j], mp[i - 1][j] + tot[i][j]);
+            tot[i - 1][j + 1] = max(tot[i - 1][j + 1], mp[i - 1][j + 1] + tot[i][j]);
+        }
+    }
+    long long ans = tot[1][1];
+    for (int i = 2; i <= m; i++)
+    {
+        ans = max(ans, tot[1][i]);
+    }
+    cout << ans << endl;
     return 0;
 }
