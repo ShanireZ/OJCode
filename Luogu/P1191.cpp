088cb2844e7a11c8c8e1e6d3fb08f71d38e7f8ca
ShanireZ <shanire@skylinejoy.com>
2022-06-12 11:38:12 +0800

    Daily

--- Luogu/P1191.cpp
+++ Luogu/P1191.cpp
@@ -18,7 +18,7 @@ int main()
             l[i][j] = l[i][j - 1] + 1;
             up[i][j] = up[i - 1][j] + 1;
             int now = 300;
-            for (int k = 0; k < up[i][j]; k++)
+            for (int k = 0; k < up[i][j]; k++) // 可用单调栈优化 找到第一个小于该位置宽度的行号
             {
                 now = min(now, l[i - k][j]);
                 cnt += now;
