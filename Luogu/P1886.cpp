ef19c8be9d2088d0c2b6da471ce86446a14f3e7a
ShanireZ <shanire@skylinejoy.com>
2022-06-15 16:58:28 +0800

    Daily

--- Luogu/P1886.cpp
+++ Luogu/P1886.cpp
@@ -1,68 +1,47 @@
 #include <iostream>
-#include <deque>
-#include <cstdio>
+#include <queue>
 using namespace std;
-int v[1000005];
-int n, k;
 deque<int> q;
-inline int read()
-{
-	int x = 0, f = 1;
-	char ch = getchar();
-	while (!isdigit(ch))
-	{
-		if (ch == '-')
-		{
-			f = -1;
-		}
-		ch = getchar();
-	}
-	while (isdigit(ch))
-	{
-		x = x * 10 + ch - 48;
-		ch = getchar();
-	}
-	return x * f;
-}
-inline void make(int t)
-{
-	q.clear();
-	for (int i = 1; i <= n; i++)
-	{
-		if (t == 0)
-		{
-			while (q.size() && v[q.back()] >= v[i])
-			{
-				q.pop_back();
-			}
-		}
-		else if (t == 1)
-		{
-			while (q.size() && v[q.back()] <= v[i])
-			{
-				q.pop_back();
-			}
-		}
-		q.push_back(i);
-		while (q.front() < i - k + 1)
-		{
-			q.pop_front();
-		}
-		if (i >= k)
-		{
-			printf("%d ", v[q.front()]);
-		}
-	}
-	printf("\n");
-}
+int v[1000005];
 int main()
 {
-	n = read(), k = read();
-	for (int i = 1; i <= n; i++)
-	{
-		v[i] = read();
-	}
-	make(0);
-	make(1);
-	return 0;
+    int n, k;
+    cin >> n >> k;
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> v[i];
+        while (q.size() && v[q.back()] >= v[i])
+        {
+            q.pop_back();
+        }
+        q.push_back(i);
+        while (q.size() && q.front() < i - k + 1)
+        {
+            q.pop_front();
+        }
+        if (i < k)
+        {
+            continue;
+        }
+        cout << v[q.front()] << " ";
+    }
+    cout << endl;
+    for (int i = 1; i <= n; i++)
+    {
+        while (q.size() && v[q.back()] <= v[i])
+        {
+            q.pop_back();
+        }
+        q.push_back(i);
+        while (q.size() && q.front() < i - k + 1)
+        {
+            q.pop_front();
+        }
+        if (i < k)
+        {
+            continue;
+        }
+        cout << v[q.front()] << " ";
+    }
+    return 0;
 }
