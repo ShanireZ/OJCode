4278eeec8b8cb4697deaaa87721a10b427f4fd98
ShanireZ <shanire@skylinejoy.com>
2022-07-03 08:42:45 +0800

    Daily

--- Luogu/P3379 BZ.cpp
+++ Luogu/P3379 BZ.cpp
@@ -1,25 +1,23 @@
-#include <cstdio>
-#include <cmath>
 #include <algorithm>
+#include <cmath>
+#include <cstdio>
 using namespace std;
-int dp[500005];
-int anc[500005][25];
-int lk[1000005][2]; //存储所有路径 [0] 路径的目标点id [1] 上一条同源路径id
-int tree[500005];   //存储最后一条连接该点的路径id
-void make(int x, int step)
+int dp[500005], anc[500005][25];
+int last[500005], to[1000005], pre[1000005]; // 链式前向星
+void make(int now, int step)
 {
-    dp[x] = step;
-    for (int i = 1; i <= log2(dp[x]); i++) //将x的祖先安排到位
+    dp[now] = step;
+    for (int i = 1; i <= log2(dp[now]); i++) // 将x的祖先安排到位
     {
-        int id = anc[x][i - 1];
-        anc[x][i] = anc[id][i - 1];
+        int id = anc[now][i - 1];
+        anc[now][i] = anc[id][i - 1];
     }
-    for (int i = tree[x]; lk[i][0] != 0; i = lk[i][1]) //查找x的子节点
+    for (int i = last[now]; i != 0; i = pre[i]) // 查找x的子节点
     {
-        int id = lk[i][0];
+        int id = to[i];
         if (dp[id] == 0)
         {
-            anc[id][0] = x;
+            anc[id][0] = now;
             make(id, step + 1);
         }
     }
@@ -30,7 +28,7 @@ void lca(int a, int b)
     {
         swap(a, b);
     }
-    if (dp[a] != dp[b]) //如果a b深度不同 将a提升到b的深度
+    if (dp[a] != dp[b]) // 如果a b深度不同 将a提升到b的深度
     {
         for (int i = log2(dp[a] - dp[b]); i >= 0; i--)
         {
@@ -40,12 +38,12 @@ void lca(int a, int b)
             }
         }
     }
-    if (a == b) //如果此时a b重合
+    if (a == b) // 如果此时a b重合
     {
         printf("%d\n", a);
         return;
     }
-    for (int i = log2(dp[a]); i >= 0; i--) //不断向上 寻找lca
+    for (int i = log2(dp[a]); i >= 0; i--) // 不断向上 寻找lca
     {
         if (anc[a][i] != anc[b][i])
         {
@@ -55,7 +53,7 @@ void lca(int a, int b)
     }
     printf("%d\n", anc[a][0]);
 }
-int rd() //正数快读
+int rd() // 正数快读
 {
     char ch = getchar();
     int x = 0;
@@ -70,25 +68,24 @@ int rd() //正数快读
     }
     return x;
 }
+void addEdge(int u, int v, int eid)
+{
+    to[eid] = v, pre[eid] = last[u];
+    last[u] = eid;
+}
 int main()
 {
-    int n = rd(), m = rd(), s = rd();
-    int x, y;
+    int n = rd(), m = rd(), s = rd(), cnt = 0;
     for (int i = 1; i < n; i++)
     {
-        x = rd(), y = rd();
-        //数组模拟向前链表
-        lk[2 * i - 1][0] = y;
-        lk[2 * i - 1][1] = tree[x];
-        tree[x] = 2 * i - 1;
-        lk[2 * i][0] = x;
-        lk[2 * i][1] = tree[y];
-        tree[y] = 2 * i;
+        int x = rd(), y = rd();
+        addEdge(x, y, ++cnt);
+        addEdge(y, x, ++cnt);
     }
     make(s, 1);
     for (int i = 1; i <= m; i++)
     {
-        x = rd(), y = rd();
+        int x = rd(), y = rd();
         lca(x, y);
     }
     return 0;
