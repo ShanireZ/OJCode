52ac1543630d844f567293ea350a6323edf8dfdf
ShanireZ <shanire@skylinejoy.com>
2022-07-12 09:03:11 +0800

    Daily

--- Luogu/P1631.cpp
+++ Luogu/P1631.cpp
@@ -1,31 +1,22 @@
 #include <iostream>
 #include <queue>
-#include <vector>
 using namespace std;
-struct Node
+struct Tot
 {
-    int v, id;
-};
-struct cmp
-{
-    bool operator()(Node n1, Node n2)
+    int v, aid, bid;
+    bool operator<(const Tot nt) const
     {
-        return n1.v > n2.v;
+        return v > nt.v;
     }
 };
-priority_queue<Node, vector<Node>, cmp> c;
-int a[100005], b[100005], p[100005];
-//b中每一个元素和a中每一个元素相加，找出最小的N个
-//a和b都是有序数列，所以a[1]+b[i]表示b中每一个和a中最小相加，其中肯定会有最小值
-//如果a[1]+b[i]最小，那么输出该值，并且再将a[2]+b[i]放入优先队列，再以此类推
-//p[i]存储b中第i个元素当前应该和a中哪个元素相加
+priority_queue<Tot> q;
+int a[100005], b[100005];
 int main()
 {
     int n;
     cin >> n;
     for (int i = 1; i <= n; i++)
     {
-        p[i] = 1;
         cin >> a[i];
     }
     for (int i = 1; i <= n; i++)
@@ -34,22 +25,13 @@ int main()
     }
     for (int i = 1; i <= n; i++)
     {
-        Node tmp;
-        tmp.id = i;
-        tmp.v = a[p[i]] + b[i];
-        p[i]++;
-        c.push(tmp);
+        q.push(Tot{a[1] + b[i], 1, i});
     }
     for (int i = 1; i <= n; i++)
     {
-        cout << c.top().v << " ";
-        int id = c.top().id;
-        c.pop();
-        Node tmp;
-        tmp.id = id;
-        tmp.v = a[p[id]] + b[id];
-        p[id]++;
-        c.push(tmp);
+        cout << q.top().v << ' ';
+        q.push(Tot{a[q.top().aid + 1] + b[q.top().bid], q.top().aid + 1, q.top().bid});
+        q.pop();
     }
     return 0;
 }
