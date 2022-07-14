e6c708e94e39ec51904173195a5aca39fd6f2050
ShanireZ <shanire@skylinejoy.com>
2022-07-14 08:35:55 +0800

    Daily

--- Luogu/P1282.cpp
+++ Luogu/P1282.cpp
@@ -1,51 +1,46 @@
-#include <algorithm>
 #include <cstring>
 #include <iostream>
 using namespace std;
-int u[1005], d[1005], dp[1005][6005];
+int dp[1005][6005];
 int main()
 {
     int n, tot = 0;
     cin >> n;
+    memset(dp, 0x3f, sizeof(dp));
+    dp[0][0] = 0;
     for (int i = 1; i <= n; i++)
     {
-        cin >> u[i] >> d[i];
-        tot += u[i] + d[i];
-    }
-    memset(dp, 0x3f, sizeof(dp));
-    dp[1][u[1]] = 0;
-    dp[1][d[1]] = 1;
-    for (int i = 2; i <= n; i++)
-    {
-        for (int j = i; j <= 6 * i; j++)
+        int a, b;
+        cin >> a >> b;
+        tot += a + b;
+        for (int s = i; s <= 6000; s++)
         {
-            if (j > u[i])
+            if (s >= a)
             {
-                dp[i][j] = min(dp[i][j], dp[i - 1][j - u[i]]); //第i张不旋转
+                dp[i][s] = min(dp[i][s], dp[i - 1][s - a]);
             }
-            if (j > d[i])
+            if (s >= b)
             {
-                dp[i][j] = min(dp[i][j], dp[i - 1][j - d[i]] + 1); //第i张旋转
+                dp[i][s] = min(dp[i][s], dp[i - 1][s - b] + 1);
             }
         }
     }
-    int minv = 0x3f3f3f3f, mint = 0x3f3f3f3f;
-    for (int i = n; i <= tot; i++)
+    int ans = 0x3f3f3f3f, r = 0x3f3f3f3f;
+    for (int i = n; i <= 6000; i++)
     {
-        if (dp[n][i] >= 0x3f3f3f3f)
+        if (dp[n][i] == 0x3f3f3f3f)
         {
             continue;
         }
-        if (minv == abs(tot - i * 2))
+        if (ans > abs(tot - i * 2))
         {
-            mint = min(mint, dp[n][i]);
+            ans = abs(tot - i * 2), r = dp[n][i];
         }
-        else if (minv > abs(tot - i * 2))
+        else if (ans == abs(tot - i * 2))
         {
-            minv = abs(tot - i * 2);
-            mint = dp[n][i];
+            r = min(r, dp[n][i]);
         }
     }
-    cout << mint << endl;
+    cout << r << endl;
     return 0;
 }
