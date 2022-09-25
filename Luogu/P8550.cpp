a1972039216b83789420189d923b010344693f88
ShanireZ <shanire@skylinejoy.com>
2022-09-25 13:39:13 +0800

    Daily

--- Luogu/P8550.cpp
+++ Luogu/P8550.cpp
@@ -1,31 +1,14 @@
 #include <algorithm>
 #include <iostream>
 using namespace std;
-int a[10], ans, n, x;
-void dfs(int now, int tot, int t)
-{
-    if (now > n)
-    {
-        if (t == 0)
-        {
-            return;
-        }
-        if (tot == 0 || abs(x) % abs(tot) != 0 || x / abs(x) == tot / abs(tot))
-        {
-            ans = 1;
-        }
-        return;
-    }
-    dfs(now + 1, tot, t);
-    dfs(now + 1, tot + a[now], t + 1);
-}
+int a[10];
 int main()
 {
     int t;
     cin >> t;
     while (t--)
     {
-        ans = 0;
+        int n, x;
         cin >> n >> x;
         for (int i = 1; i <= n; i++)
         {
@@ -33,8 +16,7 @@ int main()
         }
         sort(a + 1, a + 1 + n);
         n = unique(a + 1, a + 1 + n) - (a + 1);
-        dfs(1, 0, 0);
-        if (ans)
+        if (n > 1 || x / abs(x) == a[1] / abs(a[1]) || abs(x) % abs(a[1]) != 0)
         {
             cout << "Yes" << endl;
         }
