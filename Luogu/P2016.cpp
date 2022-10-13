d5af0f53608be72b266c0358abf161da17ddfcf5
ShanireZ <shanire@skylinejoy.com>
2022-10-13 10:02:34 +0800

    Daily

--- Luogu/P2016.cpp
+++ Luogu/P2016.cpp
@@ -1,46 +1,39 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-struct Node
-{
-    vector<int> to;
-    int fa;
-};
-Node ns[1505];
-int n, root, ans[1505][2];
+int n, fa[1505], cnt[1505][2];
+vector<int> to[1505];
 void dfs(int now)
 {
-    ans[now][1] = 1, ans[now][0] = 0;
-    for (int i = 0; i < ns[now].to.size(); i++)
-    {
-        int t = ns[now].to[i];
-        if (t == ns[now].fa)
-        {
-            continue;
-        }
-        ns[t].fa = now;
-        dfs(t);
-        ans[now][0] += ans[t][1];
-        ans[now][1] += min(ans[t][0], ans[t][1]);
-    }
+	cnt[now][1] = 1;
+	for (int i = 0; i < int(to[now].size()); i++)
+	{
+		int nxt = to[now][i];
+		if (nxt == fa[now])
+		{
+			continue;
+		}
+		fa[nxt] = now;
+		dfs(nxt);
+		cnt[now][0] += cnt[nxt][1];
+		cnt[now][1] += min(cnt[nxt][0], cnt[nxt][1]);
+	}
 }
 int main()
 {
-    cin >> n;
-    for (int i = 1; i <= n; i++)
-    {
-        int now, k;
-        cin >> now >> k;
-        for (int j = 1; j <= k; j++)
-        {
-            int t;
-            cin >> t;
-            ns[now].to.push_back(t);
-            ns[t].to.push_back(now);
-        }
-    }
-    root = 0;
-    dfs(root);
-    cout << min(ans[root][0], ans[root][1]) << endl;
-    return 0;
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		int now, k, x;
+		cin >> now >> k;
+		for (int j = 1; j <= k; j++)
+		{
+			cin >> x;
+			to[now].push_back(x);
+			to[x].push_back(now);
+		}
+	}
+	dfs(0);
+	cout << min(cnt[0][0], cnt[0][1]) << endl;
+	return 0;
 }
