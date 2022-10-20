65b1dfed36ae3ecacca7a6db1b51e56d1a693473
ShanireZ <shanire@skylinejoy.com>
2022-10-20 11:42:24 +0800

    Daily

--- P_5200_USACO_19_JAN_Sleepy_Cow_Sorting_G.cpp
+++ P_5200_USACO_19_JAN_Sleepy_Cow_Sorting_G.cpp
@@ -2,23 +2,19 @@
 #include <cstring>
 #include <algorithm>
 using namespace std;
+int rk[100005];
 int main()
 {
-    //freopen("sleepy.in", "r", stdin);
-    //freopen("sleepy.out", "w", stdout);
-
     int n;
     cin >> n;
-    int rank[n];
-    memset(rank, 0, sizeof(rank));
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i <= n; i++)
     {
-        cin >> rank[i];
+        cin >> rk[i];
     }
     int step = 0;
     for (int i = n - 2; i >= 0; i--)
     {
-        if (rank[i] > rank[i + 1])
+        if (rk[i] > rk[i + 1])
         {
             step = i + 1;
             break;
