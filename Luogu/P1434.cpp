1b8a40454c90084db7cbeebaa2975ab3659454f8
ShanireZ <shanire@skylinejoy.com>
2022-11-10 11:20:27 +0800

    Daily

--- Luogu/P1434.cpp
+++ Luogu/P1434.cpp
@@ -1,65 +1,49 @@
 #include <iostream>
-#include <algorithm>
-#include <queue>
 using namespace std;
-struct Node
+int r, c, ans, h[105][105], dp[105][105], vis[105][105];
+int ms[4][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+void dfs(int x, int y)
 {
-    int x, y, h, step;
-};
-Node nodes[105][105];
-queue<Node> way;
-int pos[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    vis[x][y] = 1;
+    int way = 1;
+    for (int i = 0; i < 4; i++)
+    {
+        int nx = x + ms[i][0], ny = y + ms[i][1];
+        if (nx < 1 || ny < 1 || nx > r || ny > c || vis[nx][ny] || h[nx][ny] >= h[x][y])
+        {
+            continue;
+        }
+        if (dp[nx][ny] == 1)
+        {
+            dfs(nx, ny);
+        }
+        way = max(way, dp[nx][ny] + 1);
+    }
+    dp[x][y] = way;
+    vis[x][y] = 0;
+}
 int main()
 {
-    ios::sync_with_stdio(false);
-    int r, c;
     cin >> r >> c;
     for (int i = 1; i <= r; i++)
     {
         for (int j = 1; j <= c; j++)
         {
-            cin >> nodes[i][j].h;
-            nodes[i][j].x = i;
-            nodes[i][j].y = j;
+            cin >> h[i][j];
+            dp[i][j] = 1;
         }
     }
-    int max_step = 0;
     for (int i = 1; i <= r; i++)
     {
         for (int j = 1; j <= c; j++)
         {
-            if (nodes[i][j].step == 0)
+            if (dp[i][j] == 1)
             {
-                nodes[i][j].step = 1;
-                way.push(nodes[i][j]);
-                while (way.size() != 0)
-                {
-                    int ox = way.front().x;
-                    int oy = way.front().y;
-                    int ostep = way.front().step;
-                    if (ostep > max_step)
-                    {
-                        max_step = ostep;
-                    }
-                    for (int k = 0; k < 4; k++)
-                    {
-                        int x = ox + pos[k][0];
-                        int y = oy + pos[k][1];
-                        if (x < 1 || x > r || y < 1 || y > c)
-                        {
-                            continue;
-                        }
-                        if (nodes[x][y].step < ostep + 1 && nodes[x][y].h < nodes[ox][oy].h)
-                        {
-                            nodes[x][y].step = ostep + 1;
-                            way.push(nodes[x][y]);
-                        }
-                    }
-                    way.pop();
-                }
+                dfs(i, j);
             }
+            ans = max(ans, dp[i][j]);
         }
     }
-    cout << max_step;
+    cout << ans << endl;
     return 0;
 }
