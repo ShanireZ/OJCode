8d59bc264a9aec52d0c05c089847595236920aa7
ShanireZ <shanire@skylinejoy.com>
2022-11-13 07:54:37 +0800

    Daily

---
+++ Luogu/P8846.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    cout << 'a';
+    for (int i = 2; i <= n; i++)
+    {
+        cout << 'b';
+    }
+    return 0;
+}
