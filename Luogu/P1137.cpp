3a87d94a3a34d4d16b5b99ca605c2e18fb7646c3
ShanireZ <shanire@skylinejoy.com>
2022-11-23 11:48:01 +0800

    Daily

--- Luogu/P1137.cpp
+++ Luogu/P1137.cpp
@@ -2,47 +2,41 @@
 #include <queue>
 #include <vector>
 using namespace std;
-int ind[100005], dp[100005];
+int n, m, ind[100005], dp[100005];
 vector<int> to[100005];
 queue<int> q;
 int main()
 {
-	int n, m;
-	cin >> n >> m;
-	for (int i = 1; i <= m; i++)
-	{
-		int x, y;
-		cin >> x >> y;
-		to[x].push_back(y), ind[y]++;
-	}
-	for (int i = 1; i <= n; i++)
-	{
-		dp[i] = 1;
-	}
-	for (int i = 1; i <= n; i++)
-	{
-		if (ind[i] == 0)
-		{
-			q.push(i);
-		}
-	}
-	while (q.size())
-	{
-		int now = q.front();
-		q.pop();
-		for (int i = 0; i < int(to[now].size()); i++)
-		{
-			int nxt = to[now][i];
-			ind[nxt]--, dp[nxt] = max(dp[nxt], dp[now] + 1);
-			if (ind[nxt] == 0)
-			{
-				q.push(nxt);
-			}
-		}
-	}
-	for (int i = 1; i <= n; i++)
-	{
-		cout << dp[i] << "\n";
-	}
-	return 0;
+    cin >> n >> m;
+    for (int i = 1; i <= m; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        to[u].push_back(v), ind[v]++;
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        if (ind[i] == 0)
+        {
+            dp[i] = 1, q.push(i);
+        }
+    }
+    while (q.size())
+    {
+        int now = q.front();
+        q.pop();
+        for (int i = 0; i < int(to[now].size()); i++)
+        {
+            int nxt = to[now][i];
+            if (--ind[nxt] == 0)
+            {
+                dp[nxt] = dp[now] + 1, q.push(nxt);
+            }
+        }
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        cout << dp[i] << "\n";
+    }
+    return 0;
 }
