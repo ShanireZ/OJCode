ac91f1a1b12f55848631053bf7f59ee5686f580b
ShanireZ <shanire@skylinejoy.com>
2022-12-02 12:21:15 +0800

    Daily

--- Luogu/P8865.cpp
+++ Luogu/P8865.cpp
@@ -12,15 +12,14 @@ int main()
     {
         long long n, m, c, f, ansc = 0, ansf = 0;
         cin >> n >> m >> c >> f;
-        memset(hz, -1, sizeof(hz)), memset(thz, 0, sizeof(thz));
-        memset(thhz, 0, sizeof(thhz)), memset(h, 0, sizeof(h));
+        memset(hz, -1, sizeof(hz)), memset(thz, 0, sizeof(thz)), memset(thhz, 0, sizeof(thhz)), memset(h, 0, sizeof(h));
         char ch;
         for (int i = 1; i <= n; i++)
         {
             for (int j = 1; j <= m; j++)
             {
                 cin >> ch;
-                a[i][j] = (ch == '0') ? 0 : 1;
+                a[i][j] = ch - '0';
             }
         }
         for (int i = n; i >= 1; i--)
@@ -32,26 +31,17 @@ int main()
                     continue;
                 }
                 hz[i][j] = hz[i][j + 1] + 1;
-                if (i + 2 <= n && a[i + 1][j] == 0 && a[i + 2][j] == 0)
+                if (i + 2 <= n && a[i + 1][j] + a[i + 2][j] == 0)
                 {
                     thz[i][j] = thz[i + 1][j] + hz[i + 2][j];
                 }
-                if (i + 3 <= n && a[i + 1][j] == 0 && a[i + 2][j] == 0 && a[i + 3][j] == 0)
+                if (i + 3 <= n && a[i + 1][j] + a[i + 2][j] + a[i + 3][j] == 0)
                 {
                     h[i][j] = h[i + 1][j] + 1;
                     thhz[i][j] = thhz[i + 1][j] + hz[i + 2][j] * h[i][j];
                 }
-            }
-        }
-        for (int i = 1; i <= n; i++)
-        {
-            for (int j = 1; j <= m; j++)
-            {
-                if (hz[i][j] > 0)
-                {
-                    ansc = (ansc + hz[i][j] * thz[i][j]) % MOD;
-                    ansf = (ansf + hz[i][j] * thhz[i][j]) % MOD;
-                }
+                ansc = (ansc + hz[i][j] * thz[i][j]) % MOD;
+                ansf = (ansf + hz[i][j] * thhz[i][j]) % MOD;
             }
         }
         cout << ansc * c % MOD << ' ' << ansf * f % MOD << '\n';
