e3b5b10438a9012a155a47cfac46b7a9d92d8501
ShanireZ <shanire@skylinejoy.com>
2022-12-04 10:29:29 +0800

    Daily

--- Luogu/P2534.cpp
+++ Luogu/P2534.cpp
@@ -1,45 +1,23 @@
-#include <iostream>
 #include <algorithm>
-#include <vector>
-#include <cmath>
+#include <iostream>
 using namespace std;
-int ans[20] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
-int now[20];
-int mins, trig, n;
-vector<int> ls;
-void makels()
-{
-    sort(ls.begin(), ls.end());
-    for (int i = 1; i <= n; i++)
-    {
-        vector<int>::iterator it = lower_bound(ls.begin(), ls.end(), now[i]);
-        now[i] = it - ls.begin() + 1;
-    }
-}
-int check()
-{
-    int tot = 0;
-    for (int i = 1; i <= n; i++)
-    {
-        if (abs(now[i] - now[i + 1]) != 1)
-        {
-            tot++;
-        }
-    }
-    return tot;
-}
+int now[20], ls[20], mins, ok, n;
 void dfs(int step, int pre)
 {
-    if (check() == 0) //达成
+    int cnt = 0;
+    for (int i = 1; i <= n; i++)
     {
-        trig = 1;
+        cnt += (abs(now[i] - now[i + 1]) != 1);
+    }
+    if (cnt == 0)
+    {
+        ok = 1;
         return;
     }
-    if (check() + step > mins) //超过上限
+    if (cnt + step > mins)
     {
         return;
     }
-    //遍历
     for (int i = 2; i <= n; i++)
     {
         if (i == pre)
@@ -57,16 +35,21 @@ int main()
     for (int i = 1; i <= n; i++)
     {
         cin >> now[i];
-        ls.push_back(now[i]);
+        ls[i] = now[i];
     }
-    makels();
+    sort(ls + 1, ls + 1 + n);
+    for (int i = 1; i <= n; i++)
+    {
+        auto it = lower_bound(ls + 1, ls + 1 + n, now[i]);
+        now[i] = it - ls;
+    }
     now[n + 1] = n + 1;
     while (1)
     {
         dfs(0, 0);
-        if (trig)
+        if (ok)
         {
-            cout << mins;
+            cout << mins << endl;
             break;
         }
         mins++;
