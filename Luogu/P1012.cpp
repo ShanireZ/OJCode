2a0c1edd0a0628e3bd329ebb89bbc4ddb3b4c4dc
ShanireZ <shanire@skylinejoy.com>
2022-12-04 16:16:56 +0800

    Daily

--- Luogu/P1012.cpp
+++ Luogu/P1012.cpp
@@ -1,42 +1,24 @@
 #include <algorithm>
-#include <cstring>
 #include <iostream>
 #include <string>
 using namespace std;
+string all[30];
+bool cmp(string a, string b)
+{
+    return a + b > b + a;
+}
 int main()
 {
     int n;
     cin >> n;
-    string nums[n];
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i <= n; i++)
     {
-        cin >> nums[i];
+        cin >> all[i];
     }
-    int j;
-    sort(nums, nums + n, greater<string>());
-    for (int i = 1; i < n; i = j)
+    sort(all + 1, all + n + 1, cmp);
+    for (int i = 1; i <= n; i++)
     {
-        j = i + 1;
-        for (; i >= 1; i--)
-        {
-            int size_pre = nums[i - 1].size();
-            int size_cur = nums[i].size();
-            if (size_pre > size_cur)
-            {
-                if (nums[i - 1].find(nums[i]) == 0 && nums[i - 1][size_cur] < nums[i][0])
-                {
-                    string temp = nums[i - 1];
-                    nums[i - 1] = nums[i];
-                    nums[i] = temp;
-                }
-            }
-        }
+        cout << all[i];
     }
-    string output;
-    for (int i = 0; i < n; i++)
-    {
-        output += nums[i];
-    }
-    cout << output;
     return 0;
 }
