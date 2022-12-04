4f24b821fd5a22139dedeffce8b1683fd2121389
ShanireZ <shanire@skylinejoy.com>
2022-12-04 09:54:07 +0800

    Daily

--- Luogu/P1379.cpp
+++ Luogu/P1379.cpp
@@ -1,101 +1,66 @@
-#include <cstdio>
-#include <string>
-#include <algorithm>
+#include <iostream>
 using namespace std;
-int mins, trig;													 //* mins最短步数 trig是否达成
-int now[5][5];													 //* 当前状态
-int ans[5][5] = {{0}, {0, 1, 2, 3}, {0, 8, 0, 4}, {0, 7, 6, 5}}; //* 目标状态
-int mx[5] = {-1, 0, 0, 1};
-int my[5] = {0, -1, 1, 0};
-bool check() //* 检查是否已经到达目标状态
+int now[5][5], ans[5][5] = {{0}, {0, 1, 2, 3}, {0, 8, 0, 4}, {0, 7, 6, 5}};
+int ms[5][2] = {{0}, {1, 0}, {0, 1}, {0, -1}, {-1, 0}}, mins;
+int dfs(int x, int y, int step, int pref)
 {
-	for (int i = 1; i <= 3; i++)
-	{
-		for (int j = 1; j <= 3; j++)
-		{
-			if (now[i][j] != ans[i][j])
-			{
-				return false;
-			}
-		}
-	}
-	return true;
-}
-//* step为从起点到当前状态的步数
-//* cnt为当前错误位置的数字个数---至少需要几步才能到达目标状态
-//* 如果cnt + step > mins 说明步数肯定超出 剪枝处理
-bool astar(int step)
-{
-	int cnt = 0;
-	for (int i = 1; i <= 3; i++)
-	{
-		for (int j = 1; j <= 3; j++)
-		{
-			if (now[i][j] != ans[i][j])
-			{
-				if (cnt + step > mins)
-				{
-					return false;
-				}
-				cnt++; //* cnt个错误位置数字最少需要cnt-1步 所以先判断后自增
-			}
-		}
-	}
-	return true;
-}
-void dfs(int x, int y, int step, int pre)
-{
-	if (check()) //* 当前状态是否完成目标
-	{
-		trig = 1;
-		return;
-	}
-	if (astar(step) == false) //* 当前是否需要剪枝
-	{
-		return;
-	}
-	for (int i = 0; i < 4; i++)
-	{
-		int nx = x + mx[i], ny = y + my[i];
-		//* i+pre==3表示往返行进 回到了上一位置
-		//* 这样就不需要额外的状态数组了
-		if (nx < 1 || nx > 3 || ny < 1 || ny > 3 || i + pre == 3)
-		{
-			continue;
-		}
-		swap(now[nx][ny], now[x][y]);
-		dfs(nx, ny, step + 1, i);
-		swap(now[nx][ny], now[x][y]);
-		if (trig)
-		{
-			return;
-		}
-	}
+    int cnt = 0; // 记录当前差异个数
+    for (int i = 1; i <= 3; i++)
+    {
+        for (int j = 1; j <= 3; j++)
+        {
+            cnt += (now[i][j] != ans[i][j]);
+        }
+    }
+    if (cnt == 0) // 无差异已达成
+    {
+        return 1;
+    }
+    else if (cnt - 1 + step > mins) // A*剪枝
+    {
+        return 0;
+    }
+    for (int i = 1; i <= 4; i++)
+    {
+        int nx = x + ms[i][0], ny = y + ms[i][1];
+        if (nx > 3 || ny > 3 || nx < 1 || ny < 1 || pref + i == 5)
+        {
+            continue;
+        }
+        swap(now[x][y], now[nx][ny]);
+        int ok = dfs(nx, ny, step + 1, i);
+        swap(now[nx][ny], now[x][y]); // 回溯复原
+        if (ok)
+        {
+            return 1;
+        }
+    }
+    return 0;
 }
 int main()
 {
-	int sx, sy;
-	for (int i = 1; i <= 3; i++)
-	{
-		for (int j = 1; j <= 3; j++)
-		{
-			char ch = getchar();
-			now[i][j] = ch - '0';
-			if (now[i][j] == 0)
-			{
-				sx = i, sy = j;
-			}
-		}
-	}
-	while (1)
-	{
-		dfs(sx, sy, 0, 5);
-		if (trig)
-		{
-			printf("%d", mins);
-			break;
-		}
-		mins++; //* 从0开始枚举最小步数 可用二分优化
-	}
-	return 0;
+    int sx = 1, sy = 1;
+    for (int i = 1; i <= 3; i++)
+    {
+        for (int j = 1; j <= 3; j++)
+        {
+            char ch;
+            cin >> ch;
+            now[i][j] = ch - '0';
+            if (now[i][j] == 0)
+            {
+                sx = i, sy = j;
+            }
+        }
+    }
+    while (true)
+    {
+        if (dfs(sx, sy, 0, 10))
+        {
+            break;
+        }
+        mins++; // 限制逐步解除至最少步数
+    }
+    cout << mins << endl;
+    return 0;
 }
--- Luogu/P2324.cpp
+++ Luogu/P2324.cpp
@@ -1,77 +1,38 @@
 #include <iostream>
-#include <string>
-#include <algorithm>
 using namespace std;
-int mins, trig; //* mins最短步数 trig是否达成
-int now[7][7];	//* 当前状态
-int ans[7][7] = {{0},
-				 {0, 1, 1, 1, 1, 1},
-				 {0, 0, 1, 1, 1, 1},
-				 {0, 0, 0, 5, 1, 1},
-				 {0, 0, 0, 0, 0, 1},
-				 {0, 0, 0, 0, 0, 0}}; //* 目标状态
-int mx[8] = {-2, -2, -1, -1, 1, 1, 2, 2};
-int my[8] = {-1, 1, -2, 2, -2, 2, -1, 1};
-bool check() //* 检查是否已经到达目标状态
-{
-	for (int i = 1; i <= 5; i++)
-	{
-		for (int j = 1; j <= 5; j++)
-		{
-			if (now[i][j] != ans[i][j])
-			{
-				return false;
-			}
-		}
-	}
-	return true;
-}
-//* step为从起点到当前状态的步数
-//* cnt为当前错误位置的数字个数---至少需要几步才能到达目标状态
-//* 如果cnt + step > mins 说明步数肯定超出 剪枝处理
-bool astar(int step)
+int ms[10][2] = {{0}, {-2, -1}, {-2, 1}, {-1, -2}, {-1, 2}, {1, -2}, {1, 2}, {2, -1}, {2, 1}};
+int ans[10][10] = {{0}, {0, 1, 1, 1, 1, 1}, {0, 0, 1, 1, 1, 1}, {0, 0, 0, 5, 1, 1}, {0, 0, 0, 0, 0, 1}, {0, 0, 0, 0, 0, 0}};
+int now[10][10], mins, ok;
+void dfs(int x, int y, int step, int pref)
 {
 	int cnt = 0;
 	for (int i = 1; i <= 5; i++)
 	{
 		for (int j = 1; j <= 5; j++)
 		{
-			if (now[i][j] != ans[i][j])
-			{
-				if (cnt + step > mins)
-				{
-					return false;
-				}
-				cnt++; //* cnt个错误位置数字最少需要cnt-1步 所以先判断后自增
-			}
+			cnt += (now[i][j] != ans[i][j]);
 		}
 	}
-	return true;
-}
-void dfs(int x, int y, int step, int pre)
-{
-	if (check()) //* 当前状态是否完成目标
+	if (cnt == 0)
 	{
-		trig = 1;
+		ok = 1;
 		return;
 	}
-	if (astar(step) == false) //* 当前是否需要剪枝
+	else if (cnt - 1 + step > mins)
 	{
 		return;
 	}
-	for (int i = 0; i < 8; i++)
+	for (int i = 1; i <= 8; i++)
 	{
-		int nx = x + mx[i], ny = y + my[i];
-		//* i+pre==7表示往返行进 回到了上一位置
-		//* 这样就不需要额外的状态数组了
-		if (nx < 1 || nx > 5 || ny < 1 || ny > 5 || i + pre == 7)
+		int nx = x + ms[i][0], ny = y + ms[i][1];
+		if (nx > 5 || ny > 5 || nx < 1 || ny < 1 || i + pref == 9)
 		{
 			continue;
 		}
-		swap(now[nx][ny], now[x][y]);
+		swap(now[x][y], now[nx][ny]);
 		dfs(nx, ny, step + 1, i);
 		swap(now[nx][ny], now[x][y]);
-		if (trig)
+		if (ok)
 		{
 			return;
 		}
@@ -81,44 +42,37 @@ int main()
 {
 	int t;
 	cin >> t;
-	for (int i = 1; i <= t; i++)
+	while (t--)
 	{
-		int sx, sy;
-		mins = 0, trig = 0;
+		int sx = 1, sy = 1;
 		for (int i = 1; i <= 5; i++)
 		{
 			for (int j = 1; j <= 5; j++)
 			{
 				char ch;
 				cin >> ch;
+				now[i][j] = ch - '0';
 				if (ch == '*')
 				{
-					now[i][j] = 5;
-					sx = i, sy = j;
-				}
-				else
-				{
-					now[i][j] = ch - '0';
+					sx = i, sy = j, now[i][j] = 5;
 				}
 			}
 		}
+		ok = 0, mins = 0;
 		while (mins <= 15)
 		{
-			dfs(sx, sy, 0, -1);
-			if (trig)
+			dfs(sx, sy, 0, 100);
+			if (ok)
 			{
 				break;
 			}
-			mins++; //* 从0开始枚举最小步数 可用二分优化
+			mins++;
 		}
-		if (trig)
+		if (ok == 0)
 		{
-			cout << mins << endl;
+			mins = -1;
 		}
-		else
-		{
-			cout << -1 << endl;
-		}
+		cout << mins << endl;
 	}
 	return 0;
 }
