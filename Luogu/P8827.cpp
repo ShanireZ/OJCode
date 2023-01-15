d5a0c849e1372ae2b1392340f9f44b23aaccb28a
ShanireZ <shanire@skylinejoy.com>
2023-01-15 08:36:20 +0800

    Daily

--- Luogu/P8827.cpp
+++ Luogu/P8827.cpp
@@ -2,73 +2,57 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-struct Node
-{
-    int fa, v;
-    vector<int> ch;
-};
-Node ns[100005];
-struct Way
+#define MX 100005
+struct Edge
 {
     int x, y;
 };
-Way ways[100005];
-int val[100005];
-void dfs(int id)
+Edge es[MX];
+int v[MX], tot[MX], fa[MX];
+vector<int> g[MX];
+void dfs(int now)
 {
-    for (int i = 0; i < ns[id].ch.size(); i++)
+    for (int i = 0; i < g[now].size(); i++)
     {
-        int to = ns[id].ch[i];
-        if (to == ns[id].fa)
+        int nxt = g[now][i];
+        if (nxt != fa[now])
         {
-            continue;
+            fa[nxt] = now;
+            dfs(nxt);
+            tot[now] += tot[nxt];
         }
-        ns[to].fa = id;
-        dfs(to);
-        ns[id].v += ns[to].v;
     }
 }
-void edit(int id, int add)
+void edit(int now, int add)
 {
-    ns[id].v += add;
-    while (ns[id].fa)
+    tot[now] += add;
+    while (fa[now])
     {
-        id = ns[id].fa;
-        ns[id].v += add;
+        now = fa[now];
+        tot[now] += add;
     }
 }
 void cut(int wid)
 {
-    int a = ways[wid].x;
-    int b = ways[wid].y;
-    if (ns[a].fa == b)
+    int a = es[wid].x, b = es[wid].y;
+    if (fa[a] == b)
     {
-        ns[a].fa = 0;
-        ns[b].v -= ns[a].v;
-        while (ns[b].fa)
-        {
-            b = ns[b].fa;
-            ns[b].v -= ns[a].v;
-        }
+        swap(a, b);
     }
-    else if (ns[b].fa == a)
+    fa[b] = 0, tot[a] -= tot[b];
+    while (fa[a])
     {
-        ns[b].fa = 0;
-        ns[a].v -= ns[b].v;
-        while (ns[a].fa)
-        {
-            a = ns[a].fa;
-            ns[a].v -= ns[b].v;
-        }
+        a = fa[a];
+        tot[a] -= tot[b];
     }
 }
-void query(int id)
+void query(int now)
 {
-    while (ns[id].fa)
+    while (fa[now])
     {
-        id = ns[id].fa;
+        now = fa[now];
     }
-    cout << ns[id].v << endl;
+    cout << tot[now] << "\n";
 }
 int main()
 {
@@ -76,16 +60,15 @@ int main()
     cin >> n >> m;
     for (int i = 1; i <= n; i++)
     {
-        cin >> val[i];
-        ns[i].v = val[i];
+        cin >> v[i];
+        tot[i] = v[i];
     }
     for (int i = 1; i < n; i++)
     {
         int a, b;
         cin >> a >> b;
-        ns[a].ch.push_back(b);
-        ns[b].ch.push_back(a);
-        ways[i].x = a, ways[i].y = b;
+        g[a].push_back(b), g[b].push_back(a);
+        es[i].x = a, es[i].y = b;
     }
     dfs(1);
     for (int i = 1; i <= m; i++)
@@ -100,9 +83,8 @@ int main()
         else if (op == 2)
         {
             cin >> x >> y;
-            int add = y - val[x];
-            val[x] = y;
-            edit(x, add);
+            edit(x, y - v[x]);
+            v[x] = y;
         }
         else if (op == 3)
         {
