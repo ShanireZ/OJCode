511c707bba969deb4fe9a45bdf5af29e565e97fd
Shanire <shanire@skylinejoy.com>
2023-02-26 08:31:46 +0800

    Daily

---
+++ USACO/S23/Bronze23FEB/q1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    long long n, t, now = 1, ans = 0;
+    cin >> n >> t;
+    for (int i = 1; i <= n; i++)
+    {
+        long long d, b;
+        cin >> d >> b;
+        now = max(now, d);
+        long long x = min(b, t - now + 1);
+        ans += x, now += x;
+        if (now > t)
+        {
+            break;
+        }
+    }
+    cout << ans << "\n";
+    return 0;
+}
