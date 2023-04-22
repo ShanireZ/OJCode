5d1699bb68b59957b4298eb642e5e7078983575d
Shanire <shanire@skylinejoy.com>
2023-04-22 08:30:12 +0800

    Daily

--- Luogu/P9206.cpp
+++ Luogu/P9206.cpp
@@ -1,7 +1,7 @@
 #include <algorithm>
 #include <iostream>
 using namespace std;
-int a[100005], tmp[100005], read();
+int a[100005], read();
 int main()
 {
 	int n = read(), m = read(), ans = 0;
@@ -14,19 +14,13 @@ int main()
 		int d = read();
 		for (int j = 1; j <= d; j++) // 枚举每个组
 		{
-			int pos = 0;
 			for (int k = j; k <= n; k += d) // 枚举组内的每个元素
 			{
-				tmp[++pos] = a[k], ans++;
-				for (int x = pos; x > 1 && tmp[x] < tmp[x - 1]; x--) // 插入排序
+				ans++;
+				for (int x = k; x - d > 0 && a[x] < a[x - d]; x -= d) // 插入排序
 				{
-					swap(tmp[x], tmp[x - 1]), ans++;
+					swap(a[x], a[x - d]), ans++;
 				}
-			}
-			pos = 0;
-			for (int k = j; k <= n; k += d)
-			{
-				a[k] = tmp[++pos];
 			}
 		}
 	}
