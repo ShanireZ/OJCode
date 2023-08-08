bd8b1bd03d3f0bd83b06a295cc78ec1a53775a94
Shanire <shanire@skylinejoy.com>
2023-08-08 17:27:43 +0800

    Daily

---
+++ Luogu/P1659.cpp
@@ -0,0 +1,48 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+using namespace std;
+#define MOD 19930726
+#define MX 1000005
+long long p[MX], cnt[MX];
+string s;
+int main()
+{
+    long long n, k, mid = 0, mx = 0;
+    cin >> n >> k >> s;
+    for (int i = 0; i < (int)s.size(); i++)
+    {
+        p[i] = (i < mx ? min(p[2 * mid - i], mx - i) : 1);
+        while (i + p[i] < (int)s.size() && i - p[i] >= 0 && s[i + p[i]] == s[i - p[i]])
+        {
+            p[i]++;
+        }
+        if (i + p[i] > mx)
+        {
+            mx = i + p[i], mid = i;
+        }
+        cnt[p[i] * 2 - 1]++;
+    }
+    long long ans = 1;
+    for (int i = 999999; i >= 1; i -= 2)
+    {
+        cnt[i] += cnt[i + 2];
+        long long t = min(k, cnt[i]), base = i;
+        k -= t;
+        while (t)
+        {
+            if (t % 2 == 1)
+            {
+                ans = ans * base % MOD;
+            }
+            base = base * base % MOD, t /= 2;
+        }
+        if (k == 0)
+        {
+            cout << ans << "\n";
+            return 0;
+        }
+    }
+    cout << -1 << "\n";
+    return 0;
+}
