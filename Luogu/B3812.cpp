75bcc6cc60184d3f053a16d6ec7add56439c55d1
Shanire <shanire@skylinejoy.com>
2023-08-11 15:11:28 +0800

    Daily

---
+++ Luogu/B3812.cpp
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <iostream>
+using namespace std;
+int main()
+{
+    int opt;
+    cin >> opt;
+    if (opt == 1)
+    {
+        cout << 100000 << "\n";
+    }
+    else
+    {
+        cout << "5\n";
+        cout << "1 0 1 0 1\n";
+    }
+    return 0;
+}
