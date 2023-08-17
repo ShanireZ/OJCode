aa7771e49bcb91946dc3f97ff810aefe1e6f8591
Shanire <shanire@skylinejoy.com>
2023-08-17 14:50:59 +0800

    Daily

---
+++ Luogu/B3821.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int opt;
+    cin >> opt;
+    if (opt == 1)
+    {
+        cout << "1000000000 999999999\n";
+    }
+    else
+    {
+        cout << "ac\n";
+    }
+    return 0;
+}
+// TAG: HACK
