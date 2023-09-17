016e4c8a38216bde9296016b63f11858198c0bc9
Shanire <shanire@skylinejoy.com>
2023-09-17 12:41:45 +0800

    Daily

--- Luogu/P1908.cpp
+++ Luogu/P1908.cpp
@@ -1,46 +1,37 @@
 #include <cstring>
 #include <iostream>
 using namespace std;
-int a[500005], tmp[500005];
-long long ans;
-int read()
-{
-    int ans = 0;
-    char ch = getchar();
-    while (ch < '0' || ch > '9')
-    {
-        ch = getchar();
-    }
-    while (ch >= '0' && ch <= '9')
-    {
-        ans = ans * 10 + ch - '0';
-        ch = getchar();
-    }
-    return ans;
-}
-long long mergesort(int l, int r)
+long long cnt, n, a[500005], tmp[500005];
+void dfs(int l, int r)
 {
     if (l == r)
     {
         return;
     }
     int mid = (l + r) / 2;
-    mergesort(l, mid), mergesort(mid + 1, r);
-    int p1 = l, p2 = mid + 1;
-    for (int i = l; i <= r; i++)
+    dfs(l, mid), dfs(mid + 1, r);
+    for (int i = l, p1 = l, p2 = mid + 1; i <= r; i++)
     {
-        (p1 <= mid && p2 <= r && a[p1] <= a[p2] || p2 > r) ? (tmp[i] = a[p1++]) : (ans += mid - p1 + 1, tmp[i] = a[p2++]);
+        if ((p1 <= mid && p2 <= r && a[p1] <= a[p2]) || p2 > r)
+        {
+            tmp[i] = a[p1++];
+        }
+        else
+        {
+            cnt += (mid + 1 - p1);
+            tmp[i] = a[p2++];
+        }
     }
     memcpy(a + l, tmp + l, sizeof(int) * (r - l + 1));
 }
 int main()
 {
-    int n = read();
+    cin >> n;
     for (int i = 1; i <= n; i++)
     {
-        a[i] = read();
+        cin >> a[i];
     }
-    mergesort(1, n);
-    printf("%lld\n", ans);
+    dfs(1, n);
+    cout << cnt << endl;
     return 0;
 }
