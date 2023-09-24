2f1d809801d1c7ece12a7e82a59f4c28996228ae
ShanireZ <shanire@skylinejoy.com>
2023-09-24 21:11:58 +0800

    Daily

--- Luogu/P1508.cpp
+++ Luogu/P1508.cpp
@@ -1,34 +1,37 @@
-#include <cstring>
 #include <iostream>
 using namespace std;
-long long mp[205][205], tot[205][205];
+long long eg[205][205], dp[205][205];
 int main()
 {
-    int n, m;
-    cin >> n >> m;
-    memset(mp, 128, sizeof(mp));
-    for (int i = 1; i <= n; i++)
+    int m, n;
+    cin >> m >> n;
+    for (int i = 1; i <= m; i++)
     {
-        for (int j = 1; j <= m; j++)
+        for (int j = 1; j <= n; j++)
         {
-            cin >> mp[i][j];
+            cin >> eg[i][j];
+            dp[i][j] = -1e18;
         }
     }
-    for (int i = n + 1; i >= 1; i--)
+    // 初始位置在最底下的中间，也就是第m+1行的n/2+1列
+    int L = n / 2 + 1, R = n / 2 + 1;
+    for (int i = m; i >= 1; i--)
     {
-        int st = max(m / 2 - (n - i), 1);
-        int ed = min(m / 2 + 2 + (n - i), m);
-        for (int j = st; j <= ed; j++)
+        if (L > 1) // 每向上走一行，两侧可行范围扩大
         {
-            tot[i - 1][j - 1] = max(tot[i - 1][j - 1], mp[i - 1][j - 1] + tot[i][j]);
-            tot[i - 1][j] = max(tot[i - 1][j], mp[i - 1][j] + tot[i][j]);
-            tot[i - 1][j + 1] = max(tot[i - 1][j + 1], mp[i - 1][j + 1] + tot[i][j]);
+            L--, R++;
+        }
+        for (int j = L; j <= R; j++)
+        {
+            // 当前这个位置是第i行第j列，可以从底下第i+1行的j-1,j,j+1列走过来
+            long long from = max(max(dp[i + 1][j - 1], dp[i + 1][j]), dp[i + 1][j + 1]);
+            dp[i][j] = from + eg[i][j];
         }
     }
-    long long ans = tot[1][1];
-    for (int i = 2; i <= m; i++)
+    long long ans = -1e18;
+    for (int i = L; i <= R; i++)
     {
-        ans = max(ans, tot[1][i]);
+        ans = max(ans, dp[1][i]);
     }
     cout << ans << endl;
     return 0;
