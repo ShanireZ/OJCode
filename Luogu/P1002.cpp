dc861ba9dc6763141313012fbab67981fa0a9707
Shanire <shanire@skylinejoy.com>
2023-09-24 16:13:09 +0800

    Daily

--- Luogu/P1002.cpp
+++ Luogu/P1002.cpp
@@ -1,36 +1,29 @@
+#include <algorithm>
 #include <iostream>
 using namespace std;
-long long mp[25][25];
-int h[10][2] = {{0, 0}, {1, 2}, {-1, 2}, {1, -2}, {-1, -2}, {2, 1}, {-2, 1}, {2, -1}, {-2, -1}};
+long long dp[25][25];
+int py[10][2] = {{0, 0}, {1, 2}, {2, 1}, {-1, 2}, {-2, 1}, {1, -2}, {2, -1}, {-1, -2}, {-2, -1}};
 int main()
 {
-    int fx, fy, hx, hy;
-    cin >> fx >> fy >> hx >> hy;
-    fx++, fy++, hx++, hy++;
-    for (int i = 0; i <= 8; i++)
+    int n, m, cx, cy;
+    cin >> n >> m >> cx >> cy;
+    dp[1][1] = 1, cx++, cy++;
+    for (int i = 1; i <= n + 1; i++)
     {
-        int x = hx + h[i][0], y = hy + h[i][1];
-        mp[x][y] = -1;
-    }
-    mp[1][1] = 1;
-    for (int i = 1; i <= fx; i++)
-    {
-        for (int j = 1; j <= fy; j++)
+        for (int j = 1; j <= m + 1; j++)
         {
-            if (mp[i][j] < 0)
+            dp[i][j] += dp[i - 1][j] + dp[i][j - 1];
+            for (int k = 0; k < 9; k++)
             {
-                continue;
-            }
-            if (mp[i - 1][j] > 0)
-            {
-                mp[i][j] += mp[i - 1][j];
-            }
-            if (mp[i][j - 1] > 0)
-            {
-                mp[i][j] += mp[i][j - 1];
+                int nx = cx + py[k][0], ny = cy + py[k][1];
+                if (nx == i && ny == j)
+                {
+                    dp[i][j] = 0;
+                    break;
+                }
             }
         }
     }
-    cout << mp[fx][fy] << endl;
+    cout << dp[n + 1][m + 1] << endl;
     return 0;
 }
