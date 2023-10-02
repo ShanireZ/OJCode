7f93090b7329d830833eb95505414443a29af300
Shanire <shanire@skylinejoy.com>
2023-10-02 08:20:31 +0800

    Daily

--- Luogu/P1948.cpp
+++ Luogu/P1948.cpp
@@ -1,88 +1,60 @@
-#include <cstdio>
 #include <algorithm>
+#include <cstring>
+#include <iostream>
 #include <queue>
 #include <vector>
-#include <cstring>
 using namespace std;
-int read()
-{
-    int ans = 0;
-    char ch = getchar();
-    while (ch > '9' || ch < '0')
-    {
-        ch = getchar();
-    }
-    while (ch >= '0' && ch <= '9')
-    {
-        ans = ans * 10 + ch - '0';
-        ch = getchar();
-    }
-    return ans;
-}
 struct Edge
 {
-    int to, l, pre;
+    int v, w;
 };
-Edge es[2000005];
-int dis[1005][1005], last[1005], n, p, k, pos;
-void addEdge(int u, int v, int w)
-{
-    es[++pos].pre = last[u];
-    es[pos].to = v, es[pos].l = w;
-    last[u] = pos;
-}
+vector<Edge> es[1005];
 struct Path
 {
-    int id, w, times;
-    Path(int Id, int W, int Times)
-    {
-        id = Id, w = W, times = Times;
-    }
-    bool operator<(const Path oth) const
+    int v, w, t;
+    bool operator<(const Path &oth) const
     {
         return w > oth.w;
     }
 };
 priority_queue<Path> q;
-void dijkstra(int st)
-{
-    memset(dis, -1, sizeof(dis));
-    q.push(Path(st, 0, 0));
-    while (q.size())
-    {
-        int from = q.top().id, dfrom = q.top().w, times = q.top().times;
-        q.pop();
-        if (dis[from][times] != -1)
-        {
-            continue;
-        }
-        dis[from][times] = dfrom;
-        for (int i = last[from]; i != 0; i = es[i].pre)
-        {
-            int to = es[i].to, dto = es[i].l;
-            //使用免费机会
-            if (times < k && dis[to][times + 1] == -1)
-            {
-                q.push(Path(to, dfrom, times + 1));
-            }
-            //不使用免费机会
-            if (dis[to][times] == -1)
-            {
-                q.push(Path(to, max(dfrom, dto), times));
-            }
-        }
-    }
-}
+int maxw[1005][1005];
 int main()
 {
-    n = read(), p = read(), k = read();
-    for (int i = 1; i <= p; i++)
+    int n, m, k;
+    cin >> n >> m >> k;
+    for (int i = 1; i <= m; i++)
     {
-        int a = read(), b = read(), l = read();
-        addEdge(a, b, l);
-        addEdge(b, a, l);
+        int a, b, c;
+        cin >> a >> b >> c;
+        es[a].push_back(Edge{b, c}), es[b].push_back(Edge{a, c});
     }
-    dijkstra(1);
-    printf("%d\n", dis[n][k]);
+    memset(maxw, 0x3f, sizeof(maxw));
+    q.push(Path{1, 0, 0}), maxw[1][0] = 0;
+    while (q.size())
+    {
+        int now = q.top().v, t = q.top().t;
+        q.pop();
+        for (Edge e : es[now])
+        {
+            int nxt = e.v, w = e.w;
+            if (maxw[nxt][t] > max(maxw[now][t], w))
+            {
+                maxw[nxt][t] = max(maxw[now][t], w);
+                q.push(Path{nxt, maxw[nxt][t], t});
+            }
+            if (t + 1 <= k && maxw[nxt][t + 1] > maxw[now][t])
+            {
+                maxw[nxt][t + 1] = maxw[now][t];
+                q.push(Path{nxt, maxw[nxt][t + 1], t + 1});
+            }
+        }
+    }
+    int ans = 0x3f3f3f3f;
+    for (int i = 0; i <= k; i++)
+    {
+        ans = min(ans, maxw[n][i]);
+    }
+    cout << (ans != 0x3f3f3f3f ? ans : -1) << endl;
     return 0;
 }
