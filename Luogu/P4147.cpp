bd979ebf2e4a5c72e7e34c8ca7e35d2161ebcc3d
Shanire <shanire@skylinejoy.com>
2023-10-04 11:37:45 +0800

    #

--- Luogu/P4147.cpp
+++ Luogu/P4147.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 using namespace std;
-int l[1005][1005], r[1005][1005], up[1005][1005];
-char mp[1005][1005];
+int l[1005][1005], r[1005][1005], up[1005][1005], mp[1005][1005];
 int main()
 {
     int n, m, ans = 0;
@@ -10,22 +9,17 @@ int main()
     {
         for (int j = 1; j <= m; j++)
         {
-            cin >> mp[i][j];
-            if (mp[i][j] == 'F')
+            char ch;
+            cin >> ch;
+            if (ch == 'F')
             {
-                up[i][j] = l[i][j] = r[i][j] = 1;
+                mp[i][j] = r[i][j] = 1;
+                l[i][j] = l[i][j - 1] + 1, up[i][j] = up[i - 1][j] + 1;
             }
         }
-        for (int j = 2; j <= m; j++)
+        for (int j = m; j >= 1; j--)
         {
-            if (mp[i][j] == mp[i][j - 1])
-            {
-                l[i][j] += l[i][j - 1];
-            }
-        }
-        for (int j = m - 1; j >= 1; j--)
-        {
-            if (mp[i][j] == mp[i][j + 1])
+            if (mp[i][j] == 1)
             {
                 r[i][j] += r[i][j + 1];
             }
@@ -35,9 +29,8 @@ int main()
     {
         for (int j = 1; j <= m; j++)
         {
-            if (mp[i][j] == mp[i - 1][j])
+            if (up[i][j] > 1)
             {
-                up[i][j] += up[i - 1][j];
                 l[i][j] = min(l[i][j], l[i - 1][j]);
                 r[i][j] = min(r[i][j], r[i - 1][j]);
             }
