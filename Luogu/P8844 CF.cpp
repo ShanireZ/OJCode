e41b9c13934353062ff615f7e4df9af6f0668767
Shanire <shanire@skylinejoy.com>
2023-10-06 11:35:06 +0800

    Daily

--- Luogu/P8844 CF.cpp
+++ Luogu/P8844 CF.cpp
@@ -1,53 +1,14 @@
-#include <cstdio>
 #include <algorithm>
+#include <iostream>
 #include <vector>
 using namespace std;
-inline int read()
+struct WT
 {
-    int ans = 0;
-    char ch = getchar();
-    while (ch > '9' || ch < '0')
-    {
-        ch = getchar();
-    }
-    while (ch >= '0' && ch <= '9')
-    {
-        ans = ans * 10 + ch - '0';
-        ch = getchar();
-    }
-    return ans;
-}
-// 10 10
-// 1 2
-// 1 3
-// 2 4
-// 2 5
-// 2 6
-// 5 8
-// 3 7
-// 7 9
-// 7 10
-// 1 2
-// 2 2
-// 2 7
-// 2 8
-// 1 4
-// 2 3
-// 2 5
-// 1 3
-// 2 10
-// 2 6
-// 问题离线存入点中,树状数组维护当前每个深度点的个数
-// 当前点子树处理完时的状态 - 当前点子树处理前的状态 = 当前点子树的信息
-// 用的树上差分的思路,其实也可以主席树,不过杀鸡用牛刀了
-int n, m, cnt, pos, tree[100005], qdp[100005], ans[100005];
-struct Node
-{
-    int fa;
-    vector<int> son;
-    vector<int> qid;
+    int dp, ans;
 };
-Node ns[100005];
+WT wts[100005];
+int n, m, pos, cnt[100005];
+vector<int> to[100005], wt[100005];
 int lowbit(int x)
 {
     return x & -x;
@@ -56,73 +17,68 @@ void edit(int x)
 {
     while (x <= n)
     {
-        tree[x]++;
+        cnt[x]++;
         x += lowbit(x);
     }
 }
 int query(int x)
 {
     int ans = 0;
-    while (x)
+    while (x > 0)
     {
-        ans += tree[x];
+        ans += cnt[x];
         x -= lowbit(x);
     }
     return ans;
 }
-void dfs(int now, int deep)
+void dfs(int now, int deep, int fa)
 {
-    for (int i = 0; i < ns[now].qid.size(); i++)
+    for (int id : wt[now])
     {
-        int id = ns[now].qid[i];
-        int dp = qdp[id];
-        ans[id] = query(n) - query(dp - 1);
+        wts[id].ans = query(n) - query(wts[id].dp - 1);
     }
-    for (int i = 0; i < ns[now].son.size(); i++)
+    edit(deep);
+    for (int nxt : to[now])
     {
-        int s = ns[now].son[i];
-        if (s == ns[now].fa)
+        if (nxt == fa)
         {
             continue;
         }
-        ns[s].fa = now;
-        dfs(s, deep + 1);
+        dfs(nxt, deep + 1, now);
     }
-    edit(deep);
-    for (int i = 0; i < ns[now].qid.size(); i++)
+    for (int id : wt[now])
     {
-        int id = ns[now].qid[i];
-        int dp = qdp[id];
-        ans[id] = query(n) - query(dp - 1) - ans[id];
+        wts[id].ans = query(n) - query(wts[id].dp - 1) - wts[id].ans;
     }
 }
 int main()
 {
-    n = read(), m = read();
+    cin >> n >> m;
     for (int i = 1; i < n; i++)
     {
-        int x = read(), y = read();
-        ns[x].son.push_back(y);
-        ns[y].son.push_back(x);
+        int x, y;
+        cin >> x >> y;
+        to[x].push_back(y), to[y].push_back(x);
     }
-    int dp = n + 1;
+    int yellow = n + 1;
     for (int i = 1; i <= m; i++)
     {
-        int op = read(), x = read();
+        int op, x;
+        cin >> op >> x;
         if (op == 1)
         {
-            dp = x;
+            yellow = x;
         }
         else
         {
-            ns[x].qid.push_back(++cnt);
-            qdp[cnt] = dp;
+            wts[++pos] = WT{yellow, 0};
+            wt[x].push_back(pos);
         }
     }
-    dfs(1, 1);
-    for (int i = 1; i <= cnt; i++)
+    dfs(1, 1, 0);
+    for (int i = 1; i <= pos; i++)
     {
-        printf("%d\n", ans[i]);
+        cout << wts[i].ans << endl;
     }
     return 0;
 }
