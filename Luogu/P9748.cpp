22e1e7c4bf122cf15c2308c7d70137b9c1002a2b
Shanire <shanire@skylinejoy.com>
2023-10-21 13:55:04 +0800

    Daily

---
+++ Luogu/P9748.cpp
@@ -0,0 +1,20 @@
+#include <cmath>
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int now = n, ans1 = 0, ans2 = 0;
+    while (now)
+    {
+        ans1++;
+        if (now % 3 == 1 && ans2 == 0)
+        {
+            ans2 = ans1;
+        }
+        now -= ceil(now / 3.0);
+    }
+    cout << ans1 << " " << ans2 << endl;
+    return 0;
+}
---
+++ Luogu/P9749.cpp
@@ -0,0 +1,26 @@
+#include <cmath>
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+int v[100005];
+priority_queue<int, vector<int>, greater<int>> q;
+int main()
+{
+    long long n, d, ans = 0, ex = 0;
+    cin >> n >> d;
+    for (int i = 1; i < n; i++)
+    {
+        cin >> v[i];
+    }
+    for (int i = 1; i < n; i++)
+    {
+        int a;
+        cin >> a;
+        q.push(a);
+        long long x = ceil(1.0 * (v[i] - ex) / d);
+        ans += x * q.top(), ex += x * d - v[i];
+    }
+    cout << ans << endl;
+    return 0;
+}
