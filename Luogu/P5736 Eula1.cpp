bee4c9319439c526d172a1dcb15a2f88fce7ff09
Shanire <shanire@skylinejoy.com>
2023-11-08 12:03:07 +0800

    Daily

---
+++ Luogu/P5736 Eula1.cpp
@@ -0,0 +1,33 @@
+#include <algorithm>
+#include <iostream>
+using namespace std;
+int prim[100005], vis[100005], pos, n, x;
+int main()
+{
+    vis[0] = vis[1] = 1;
+    for (int i = 2; i <= 100000; i++)
+    {
+        if (vis[i] == 0)
+        {
+            prim[++pos] = i;
+        }
+        for (int j = 1; j <= pos && i * prim[j] <= 100000; j++)
+        {
+            if (prim[j] > i)
+            {
+                break;
+            }
+            vis[i * prim[j]] = 1;
+        }
+    }
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> x;
+        if (vis[x] == 0)
+        {
+            cout << x << " ";
+        }
+    }
+    return 0;
+}
--- Luogu/P5736.cpp
+++ Luogu/P5736.cpp
@@ -1,28 +1,25 @@
+#include <cmath>
 #include <iostream>
-#include <algorithm>
 using namespace std;
-int prime[100005];
-void makep(int max)
-{
-    for (int i = 2; i <= max / 2 + 1; i++)
-    {
-        for (int j = 2; i * j <= max; j++)
-        {
-            prime[i * j] = 1;
-        }
-    }
-}
+int vis[100005], n, num;
 int main()
 {
-    int n;
+    vis[0] = vis[1] = 1;
+    for (int i = 2; i * i <= 100000; i++)
+    {
+        if (vis[i] == 0)
+        {
+            for (int j = 2; i * j <= 100000; j++)
+            {
+                vis[i * j] = 1;
+            }
+        }
+    }
     cin >> n;
-    prime[0] = prime[1] = 1;
-    makep(100000);
     for (int i = 1; i <= n; i++)
     {
-        int num;
         cin >> num;
-        if (prime[num] == 0)
+        if (vis[num] == 0)
         {
             cout << num << " ";
         }
