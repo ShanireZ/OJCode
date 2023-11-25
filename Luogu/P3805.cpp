e71bdf85e351d380366b9bb9fedf1ea88b2ee9f5
Shanire <shanire@skylinejoy.com>
2023-11-25 15:02:40 +0800

    Daily

--- Luogu/P3805.cpp
+++ Luogu/P3805.cpp
@@ -1,38 +1,29 @@
 #include <algorithm>
-#include <cstdio>
-#include <string>
+#include <iostream>
 using namespace std;
-int p[11000005], pos = 1, ans;
-char s[22000005];
-void manacher()
+char s[22000005], ch;
+int p[22000005], pos, ans = 1, mid = 1, mx = 2;
+int main()
 {
-    int mx = 0, mid = 0;
-    for (int i = 0; i < pos; i++)
+    s[++pos] = '#', ch = getchar();
+    while (ch >= 'a' && ch <= 'z')
     {
-        p[i] = (i < mx) ? min(p[mid * 2 - i], mx - i) : 1;
-        while (i + p[i] < pos && i - p[i] >= 0 && s[i + p[i]] == s[i - p[i]])
+        s[++pos] = ch, s[++pos] = '#', ch = getchar();
+    }
+    for (int i = 1; i <= pos; i++)
+    {
+        p[i] = (i < mx ? min(mx - i, p[2 * mid - i]) : 1);
+        while (i + p[i] <= pos && i - p[i] >= 1 && s[i + p[i]] == s[i - p[i]])
         {
             p[i]++;
         }
-        if (i + p[i] > mx)
+        if (mx < i + p[i])
         {
-            mx = i + p[i];
-            mid = i;
+            mid = i, mx = i + p[i];
         }
-        ans = max(ans, p[i]);
+        ans = max(ans, p[i] - 1);
     }
-}
-int main()
-{
-    char ch = getchar();
-    s[0] = '#';
-    while (ch >= 'a' && ch <= 'z')
-    {
-        s[pos++] = ch;
-        s[pos++] = '#';
-        ch = getchar();
-    }
-    manacher();
-    printf("%d\n", ans - 1);
+    cout << ans << endl;
     return 0;
 }
+// TAG: manacher 最长回文子串
