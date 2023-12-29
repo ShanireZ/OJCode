1a3f349deb1b4aebebff8cc846c634386c6f2846
Shanire <shanire@skylinejoy.com>
2023-12-29 09:31:26 +0800

    Daily

--- Luogu/B3879.cpp
+++ Luogu/B3879.cpp
@@ -3,19 +3,28 @@
 using namespace std;
 int main()
 {
-    ios::sync_with_stdio(false);
     long long n, k, ans = 0;
     cin >> n >> k;
+    // 设x为sum/k，如果k为奇数，那么sum=kx；如果k为偶数，那么sum=(2x+1)(k/2)
+    // 如果sum是完全平方数，那么sum的两个组成部分肯定一个是ab^2、另一个是ac^2 的形式
+    long long hf = (k % 2 == 1 ? k : k / 2);
+    for (long long i = 2; hf >= i * i; i++)
+    {
+        while (hf % (i * i) == 0)
+        {
+            hf /= i * i;
+        }
+    }
     if (k % 2 == 1) // sum=kx
     {
         for (long long i = 1;; i++)
         {
-            long long x = i * i / k;
-            if (x + k / 2 > n) // 右边界
+            long long x = i * i * hf;
+            if (x + k / 2 > n) // 右边界检查
             {
                 break;
             }
-            if (i * i % k == 0 && x - k / 2 >= 1)
+            else if (x - k / 2 >= 1) // 左边界检查
             {
                 ans++;
             }
@@ -23,16 +32,7 @@ int main()
     }
     else // sum=(2x+1)(k/2)
     {
-        // 如果sum是完全平方数，那么sum的两个组成部分肯定一个是ab^2、另一个是ac^2 的形式
-        long long hf = k / 2;
-        for (long long i = 2; hf >= i * i; i++)
-        {
-            while (hf % (i * i) == 0)
-            {
-                hf /= i * i;
-            }
-        }
-        // 如果k/2剩余的a是偶数，那么2x+1也为偶数
+        // 如果k/2中的的a是偶数，那么2x+1也为偶数
         // 但2x+1必为奇数，所以产生冲突此情况无解
         if (hf % 2 == 0)
         {
@@ -42,12 +42,12 @@ int main()
         //  2x+1=ab^2且a为奇数2x+1也是奇数，所以b^2必须为奇数
         for (long long i = 1;; i += 2)
         {
-            long long ano = i * i * hf;
-            if ((ano - 1) / 2 + k / 2 > n) // 右边界
+            long long x = (i * i * hf - 1) / 2;
+            if (x + k / 2 > n) // 右边界检查
             {
                 break;
             }
-            else if (ano > k) // 左边界
+            else if (x - k / 2 >= 0) // 左边界检查
             {
                 ans++;
             }
