e600f5a9874307d74e38c1cd61eaa92957684fe6
Shanire <shanire@skylinejoy.com>
2024-01-21 16:12:11 +0800

    Daily

--- Luogu/B3865.cpp
+++ Luogu/B3865.cpp
@@ -5,49 +5,18 @@ int main()
 {
     int n;
     cin >> n;
-    for (int i = 0; i < n / 2; i++)
+    for (int i = 1, p1 = 1, p2 = n; i <= n; i++, p1++, p2--)
     {
-        for (int j = 1; j <= i; j++)
+        for (int j = 1; j <= n; j++)
         {
-            cout << '-';
-        }
-        cout << '+';
-        for (int j = 1; j <= n - 2 - i * 2; j++)
-        {
-            cout << '-';
-        }
-        cout << '+';
-        for (int j = 1; j <= i; j++)
-        {
-            cout << '-';
-        }
-        cout << endl;
-    }
-    for (int i = 1; i <= n / 2; i++)
-    {
-        cout << '-';
-    }
-    cout << '+';
-    for (int i = 1; i <= n / 2; i++)
-    {
-        cout << '-';
-    }
-    cout << endl;
-    for (int i = n / 2 - 1; i >= 0; i--)
-    {
-        for (int j = 1; j <= i; j++)
-        {
-            cout << '-';
-        }
-        cout << '+';
-        for (int j = 1; j <= n - 2 - i * 2; j++)
-        {
-            cout << '-';
-        }
-        cout << '+';
-        for (int j = 1; j <= i; j++)
-        {
-            cout << '-';
+            if (j == p1 || j == p2)
+            {
+                cout << '+';
+            }
+            else
+            {
+                cout << '-';
+            }
         }
         cout << endl;
     }
