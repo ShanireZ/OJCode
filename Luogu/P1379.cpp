9192a3c87e1fc9f9e1dda843b047de1187338fa7
Shanire <shanire@skylinejoy.com>
2024-01-31 11:29:08 +0800

    Daily

--- Luogu/P1379.cpp
+++ Luogu/P1379.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 using namespace std;
 int now[5][5], ans[5][5] = {{0}, {0, 1, 2, 3}, {0, 8, 0, 4}, {0, 7, 6, 5}};
-int ms[5][2] = {{0}, {1, 0}, {0, 1}, {0, -1}, {-1, 0}}, mins;
-int dfs(int x, int y, int step, int pref)
+int ms[5][2] = {{0}, {1, 0}, {0, 1}, {0, -1}, {-1, 0}}, lim, sx, sy;
+int dfs(int x, int y, int step, int f)
 {
     int cnt = 0; // 记录当前差异个数
     for (int i = 1; i <= 3; i++)
@@ -16,14 +16,14 @@ int dfs(int x, int y, int step, int pref)
     {
         return 1;
     }
-    else if (cnt - 1 + step > mins) // A*剪枝
+    if (cnt - 1 + step > lim) // A*剪枝
     {
         return 0;
     }
     for (int i = 1; i <= 4; i++)
     {
         int nx = x + ms[i][0], ny = y + ms[i][1];
-        if (nx > 3 || ny > 3 || nx < 1 || ny < 1 || pref + i == 5)
+        if (nx > 3 || ny > 3 || nx < 1 || ny < 1 || f + i == 5)
         {
             continue;
         }
@@ -39,7 +39,6 @@ int dfs(int x, int y, int step, int pref)
 }
 int main()
 {
-    int sx = 1, sy = 1;
     for (int i = 1; i <= 3; i++)
     {
         for (int j = 1; j <= 3; j++)
@@ -57,10 +56,10 @@ int main()
     {
         if (dfs(sx, sy, 0, 10))
         {
+            cout << lim << endl;
             break;
         }
-        mins++; // 限制逐步解除至最少步数
+        lim++; // 限制逐步解除至最少步数
     }
-    cout << mins << endl;
     return 0;
 }
