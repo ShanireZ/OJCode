8a30d8a30c18b4c179ac2c93b9a864b120287dda
Shanire <shanire@skylinejoy.com>
2024-02-07 14:18:26 +0800

    Daily

--- Luogu/P5367.cpp
+++ Luogu/P5367.cpp
@@ -1,51 +1,46 @@
-#include <iostream>
 #include <algorithm>
+#include <iostream>
 using namespace std;
-int tree[1000005], n;
-long long fact[1000005], mod = 998244353, ans;
-//! 康托展开公式 (i = 1~n)∑ ((a_i - 1) * (n - i)!)
-//! i表示从左到右第几位 a_i表示当前位的数字是剩余数字中第几大
-//! 1 ~ (i - 1) 已经排好的前提下 第i位比当前数字还小的有(a_i - 1)种
-//! 右侧还有(n - i)个数字 可形成(n - i)!种排列
-//! 那么当前位就会有(a_i - 1) * (n - i)!种更小的可能
-int lowbit(int x)
+long long jc[1000005], tree[1000005], mod = 998244353, n, ans;
+long long lowbit(long long x)
 {
     return x & -x;
 }
-void edit(int id, int add)
+void edit(long long pos, long long k)
 {
-    while (id <= n)
+    while (pos <= n)
     {
-        tree[id] += add;
-        id += lowbit(id);
+        tree[pos] += k;
+        pos += lowbit(pos);
     }
 }
-int query(int id)
+long long query(int pos)
 {
-    int ans = 0;
-    while (id)
+    long long ans = 0;
+    while (pos)
     {
-        ans += tree[id];
-        id -= lowbit(id);
+        ans += tree[pos];
+        pos -= lowbit(pos);
     }
     return ans;
 }
 int main()
 {
     cin >> n;
-    fact[0] = 1;
+    jc[0] = 1;
     for (int i = 1; i <= n; i++)
     {
-        fact[i] = fact[i - 1] * i % mod;
+        jc[i] = jc[i - 1] * i % mod;
         edit(i, 1);
     }
-    for (int i = 1; i <= n; i++)
+    for (int i = n - 1; i >= 0; i--)
     {
-        int x;
+        long long x;
         cin >> x;
-        ans = (ans + (query(x) - 1) * fact[n - i] % mod) % mod;
         edit(x, -1);
+        ans = (ans + query(x) * jc[i] % mod) % mod;
     }
-    cout << (ans + 1) % mod;
+    cout << ans + 1 << endl;
     return 0;
 }
+// TAG: 康托展开 树状数组
