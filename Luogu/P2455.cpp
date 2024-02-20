acb852381ade10c6a4da2c7400ab4679f1c96d88
Shanire <shanire@skylinejoy.com>
2024-02-20 16:45:34 +0800

    Daily

--- Luogu/P2455.cpp
+++ Luogu/P2455.cpp
@@ -1,12 +1,8 @@
-#include <iostream>
-#include <cmath>
-#include <iomanip>
 #include <algorithm>
+#include <iomanip>
+#include <iostream>
 using namespace std;
-// 2
-// 0 2 3
-// 0 0 0
-double f[55][55], ans[55];
+double f[55][55];
 int main()
 {
 	int n;
@@ -18,80 +14,72 @@ int main()
 			cin >> f[i][j];
 		}
 	}
-	int now = 1;
-	for (int j = 1; j <= n; j++) //查找当前项最大系数 存到第now行
+	for (int j = 1, now = 1; j <= n; j++) // 查找当前项最大系数 存到第now行
 	{
-		int pos = now;
-		for (int i = now + 1; i <= n; i++)
+		int mpos = now;
+		for (int i = now; i <= n; i++)
 		{
-			if (fabs(f[i][j]) > fabs(f[pos][j]))
+			if (abs(f[i][j]) > abs(f[mpos][j]))
 			{
-				pos = i;
+				mpos = i;
 			}
 		}
-		if (f[pos][j] == 0)
+		if (abs(f[mpos][j]) < 1e-6)
 		{
 			continue;
 		}
-		for (int k = 1; k <= n + 1; k++) //将第j项最大系数的式子放在第now行
+		swap(f[now], f[mpos]);
+		for (int i = n + 1; i >= j; i--)
 		{
-			swap(f[pos][k], f[now][k]);
+			f[now][i] /= f[now][j];
 		}
-		for (int i = 1; i <= n; i++) //逐行消元
+		for (int i = 1; i <= n; i++) // 逐行消元
 		{
 			if (i == now)
 			{
 				continue;
 			}
-			double x = f[i][j] / f[now][j]; //当前行的第j项是第now行几倍
-			for (int k = j; k <= n + 1; k++)
+			for (int k = n + 1; k >= j; k--)
 			{
-				f[i][k] -= x * f[now][k];
+				f[i][k] -= f[i][j] * f[now][k];
 			}
 		}
 		now++;
 	}
-	for (int i = 1; i <= n; i++)
-	{
-		ans[i] = 100000000;
-	}
+	int sta = 1;
 	for (int i = 1; i <= n; i++)
 	{
 		int p = 0;
 		for (int j = 1; j <= n; j++)
 		{
-			if (fabs(f[i][j]) > 1e-6)
+			if (abs(f[i][j]) > 1e-6)
 			{
 				p = j;
 				break;
 			}
 		}
-		if (p == 0 && f[i][n + 1] != 0)
+		if (p == 0)
 		{
-			cout << -1 << endl;
-			return 0;
-		}
-		if (p != 0)
-		{
-			ans[p] = f[i][n + 1] / f[i][p];
+			if (abs(f[i][n + 1]) > 1e-6)
+			{
+				cout << -1 << endl;
+				return 0;
+			}
+			else
+			{
+				sta = 0;
+			}
 		}
 	}
-	for (int i = 1; i <= n; i++)
+	if (sta == 0)
 	{
-		if (ans[i] == 100000000)
-		{
-			cout << 0 << endl;
-			return 0;
-		}
-		if (fabs(ans[i]) < 1e-6)
-		{
-			ans[i] = 0;
-		}
+		cout << 0 << endl;
+		return 0;
 	}
 	for (int i = 1; i <= n; i++)
 	{
 		cout << "x" << i << "=";
-		cout << fixed << setprecision(2) << ans[i] << endl;
+		cout << fixed << setprecision(2) << f[i][n + 1] << endl;
 	}
 	return 0;
 }
