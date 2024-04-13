15e03ad688da0a7268b57ea279a628d8d6a7f038
Shanire <shanire@skylinejoy.com>
2024-04-13 16:09:56 +0800

    Daily

--- Luogu/P1168.cpp
+++ Luogu/P1168.cpp
@@ -6,21 +6,24 @@ priority_queue<int, vector<int>, greater<int>> qx;
 priority_queue<int, vector<int>, less<int>> qd;
 int main()
 {
-    int n;
+    int n, x;
     cin >> n;
-    for (int i = 1, p = 1; i <= n; i++)
+    for (int i = 1, sz = 1; i <= n; i++)
     {
-        int x;
         cin >> x;
         qd.push(x);
-        while ((int)qd.size() == p)
+        if (qd.size() > sz)
         {
             qx.push(qd.top()), qd.pop();
         }
         if (i % 2 == 1)
         {
-            cout << qx.top() << endl;
-            p++, qd.push(qx.top()), qx.pop();
+            cout << qd.top() << endl;
+            sz++;
+            if (qx.size())
+            {
+                qd.push(qx.top()), qx.pop();
+            }
         }
     }
     return 0;
