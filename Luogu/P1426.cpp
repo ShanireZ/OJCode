270403b2afb0df4867177afd63da61017e8bd520
Shanire <shanire@skylinejoy.com>
2024-04-14 14:18:29 +0800

    Daily

--- Luogu/P1426.cpp
+++ Luogu/P1426.cpp
@@ -1,26 +1,23 @@
+#include <algorithm>
 #include <iostream>
-#include <cmath>
 using namespace std;
 int main()
 {
-	float s, x, v = 7.0, i;
-	cin >> s >> x;
-	s = s - x;
-	
-	for(i = 0; s > 0 ; i++)
-	{
-		v = v * pow(0.98, i);
-		s = s - v;
-	}
-	
-	v = v * pow(0.98, i);
-	s = x * 2 + s;
-	if(s >= v)
-	{
-		cout << "y";
-	}else
-	{
-		cout << "n";
-	}
-	return 0;
+    double s, x, v = 7;
+    cin >> s >> x;
+    while (s - x > 0)
+    {
+        s -= v;
+        v = v * 0.98;
+    }
+    s += x;
+    if (s >= v)
+    {
+        cout << "y" << endl;
+    }
+    else
+    {
+        cout << "n" << endl;
+    }
+    return 0;
 }
