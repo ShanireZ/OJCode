700d6db2ba7fd073982afd62bb97b0c099f9b25a
Shanire <shanire@skylinejoy.com>
2024-04-14 13:38:29 +0800

    Daily

--- Luogu/P1150.cpp
+++ Luogu/P1150.cpp
@@ -3,12 +3,12 @@
 using namespace std;
 int main()
 {
-    long long n, k, ans = 0;
+    long long n, k, r = 0, ans = 0;
     cin >> n >> k;
     while (n)
     {
-        ans += n;
-        n = n / k;
+        ans += n, r += n; // 有可能之前剩余的烟头也能换烟
+        n = r / k, r = r % k;
     }
     cout << ans << endl;
     return 0;
