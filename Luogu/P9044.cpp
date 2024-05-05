2cb7b4be03cbf8be4a4793415efc09ddf7a5ea40
Shanire <shanire@skylinejoy.com>
2024-05-05 10:44:12 +0800

    Daily

--- Luogu/P9044.cpp
+++ Luogu/P9044.cpp
@@ -1,27 +1,23 @@
 #include <algorithm>
 #include <iostream>
 using namespace std;
-int a[2005];
+int a[2005], n, k;
 int main()
 {
-    int n, k;
     cin >> n >> k;
     for (int i = 1; i <= n; i++)
     {
         cin >> a[i];
     }
-    sort(a + 1, a + 1 + n);
-    for (int i = n - k; i >= 1; i--)
+    sort(a + 1, a + 1 + n, greater<int>());
+    for (int i = k + 1; i <= n; i++)
     {
-        if (a[i] == a[i + 1])
+        if (a[i] != a[i - 1])
         {
-            k++;
-        }
-        else
-        {
-            break;
+            cout << i - 1 << endl;
+            return 0;
         }
     }
-    cout << k << endl;
+    cout << n << endl;
     return 0;
 }
