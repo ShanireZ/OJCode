3bc900135cbb03ce11eecab412fde9516c7a69e9
Shanire <shanire@skylinejoy.com>
2024-05-05 10:30:56 +0800

    Daily

--- Luogu/P8833.cpp
+++ Luogu/P8833.cpp
@@ -1,26 +1,22 @@
 #include <iostream>
 using namespace std;
-int a[25];
+int a[25], n, m, x, ans;
 int main()
 {
-    int n, m;
     cin >> n >> m;
     for (int i = 1; i <= n; i++)
     {
-        int x;
         cin >> x;
         a[x] = 1;
     }
-    int tot = 0;
     for (int i = 1; i <= m; i++)
     {
-        int x;
         cin >> x;
         if (a[x] == 1)
         {
-            tot++;
+            ans++;
         }
     }
-    cout << tot;
+    cout << ans << endl;
     return 0;
 }
