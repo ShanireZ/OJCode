925d884f611c6d69aca6d4b1ebf67eb09689336b
Shanire <shanire@skylinejoy.com>
2024-05-26 13:37:25 +0800

    Daily

--- Luogu/P3397 On.cpp
+++ Luogu/P3397 On.cpp
@@ -1,30 +1,28 @@
-#include <iostream>
 #include <algorithm>
+#include <iostream>
 using namespace std;
-int cnt[1005][1005];
+int d[1005][1005], a[1005][1005];
 int main()
 {
-	int n, m;
-	cin >> n >> m;
-	for (int i = 1; i <= m; i++)
-	{
-		int x1, y1, x2, y2;
-		cin >> x1 >> y1 >> x2 >> y2;
-		for (int j = x1; j <= x2; j++)
-		{
-			cnt[j][y1]++;
-			cnt[j][y2 + 1]--;
-		}
-	}
-	for (int i = 1; i <= n; i++)
-	{
-		int num = 0;
-		for (int j = 1; j <= n; j++)
-		{
-			num += cnt[i][j];
-			cout << num << " ";
-		}
-		cout << endl;
-	}
-	return 0;
+    int n, m;
+    cin >> n >> m;
+    for (int i = 1; i <= m; i++)
+    {
+        int x1, y1, x2, y2;
+        cin >> x1 >> y1 >> x2 >> y2;
+        for (int x = x1; x <= x2; x++)
+        {
+            d[x][y1]++, d[x][y2 + 1]--;
+        }
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            a[i][j] = d[i][j] + a[i][j - 1];
+            cout << a[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
 }
