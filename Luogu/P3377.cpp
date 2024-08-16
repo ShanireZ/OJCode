ba6d1b0ad2d20afce49f938e78611bff6a9fe281
Shanire <shanire@skylinejoy.com>
2024-08-16 13:36:46 +0800

    左偏树

--- Luogu/P3377.cpp
+++ Luogu/P3377.cpp
@@ -1,99 +1,74 @@
 #include <algorithm>
-#include <cstdio>
+#include <iostream>
 using namespace std;
-int read();
 struct Node
 {
-    int v, fa, lc, rc, dis;
+	int lc, rc, v, h;
 };
 Node ns[100005];
-int n, m;
+int g[100005], n, m;
 int dfn(int x)
 {
-    return (ns[x].fa == x) ? x : ns[x].fa = dfn(ns[x].fa);
+	return x == g[x] ? x : g[x] = dfn(g[x]);
 }
 int merge(int x, int y)
 {
-    if (x == 0 || y == 0)
-    {
-        return x + y;
-    }
-    if (ns[x].v > ns[y].v || (ns[x].v == ns[y].v && x > y))
-    {
-        swap(x, y);
-    }
-    ns[x].rc = merge(ns[x].rc, y);
-    ns[ns[x].rc].fa = x;
-    if (ns[ns[x].lc].dis < ns[ns[x].rc].dis)
-    {
-        swap(ns[x].lc, ns[x].rc);
-    }
-    ns[x].dis = ns[ns[x].rc].dis + 1;
-    return x;
+	if (x == 0 || y == 0)
+	{
+		return x + y;
+	}
+	if (ns[x].v > ns[y].v)
+	{
+		swap(x, y);
+	}
+	ns[x].rc = merge(ns[x].rc, y);
+	g[ns[x].rc] = x;
+	if (ns[ns[x].lc].h < ns[ns[x].rc].h)
+	{
+		swap(ns[x].lc, ns[x].rc);
+	}
+	ns[x].h = ns[ns[x].rc].h + 1;
+	return x;
 }
-void pop(int x)
+void del(int x)
 {
-    ns[ns[x].lc].fa = ns[x].lc;
-    ns[ns[x].rc].fa = ns[x].rc;
-    ns[x].v = -1;
-    ns[x].fa = merge(ns[x].lc, ns[x].rc);
+	ns[x].v = -1;
+	g[ns[x].lc] = ns[x].lc, g[ns[x].rc] = ns[x].rc;
+	g[x] = merge(ns[x].lc, ns[x].rc);
 }
 int main()
 {
-    n = read(), m = read();
-    ns[0].dis = -1;
-    for (int i = 1; i <= n; i++)
-    {
-        ns[i].v = read();
-        ns[i].fa = i;
-    }
-    for (int i = 1; i <= m; i++)
-    {
-        int t = read();
-        if (t == 1) // merge
-        {
-            int x = read(), y = read();
-            int gx = dfn(x), gy = dfn(y);
-            if (ns[x].v == -1 || ns[y].v == -1 || gx == gy)
-            {
-                continue;
-            }
-            merge(dfn(x), dfn(y));
-        }
-        else // del & query
-        {
-            int x = read();
-            if (ns[x].v == -1)
-            {
-                printf("-1\n");
-            }
-            else
-            {
-                x = dfn(x);
-                printf("%d\n", ns[x].v);
-                pop(x);
-            }
-        }
-    }
-    return 0;
-}
-int read()
-{
-    char ch = getchar();
-    while (ch != '-' && (ch > '9' || ch < '0'))
-    {
-        ch = getchar();
-    }
-    int t = 1, ans = 0;
-    if (ch == '-')
-    {
-        t = -1;
-        ch = getchar();
-    }
-    while (ch >= '0' && ch <= '9')
-    {
-        ans = ans * 10 + ch - '0';
-        ch = getchar();
-    }
-    return ans * t;
+	cin >> n >> m;
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> ns[i].v;
+		g[i] = i;
+	}
+	for (int i = 1; i <= m; i++)
+	{
+		int opt, x, y;
+		cin >> opt >> x;
+		if (opt == 1)
+		{
+			cin >> y;
+			int gx = dfn(x), gy = dfn(y);
+			if (gx == gy || ns[x].v == -1 || ns[y].v == -1)
+			{
+				continue;
+			}
+			merge(gx, gy);
+		}
+		else
+		{
+			if (ns[x].v == -1)
+			{
+				cout << "-1\n";
+				continue;
+			}
+			int gx = dfn(x);
+			cout << ns[gx].v << endl;
+			del(gx);
+		}
+	}
+	return 0;
 }
