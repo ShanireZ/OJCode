8c17c3c3e9635935e95a35b14f0c1f0976201918
Shanire <shanire@skylinejoy.com>
2024-08-21 08:44:16 +0800

    Daily

---
+++ Luogu/B4017.cpp
@@ -0,0 +1,10 @@
+#include <algorithm>
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x, y;
+    cin >> x >> y;
+    cout << (x - 2016) * 12 + (y - 8) + 1 << endl;
+    return 0;
+}
