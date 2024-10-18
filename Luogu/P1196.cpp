ea9fce18aa0973948460deea44a4560e0ca577fb
Shanire <shanire@skylinejoy.com>
2024-10-18 10:10:28 +0800

    Update

--- Luogu/P1196.cpp
+++ Luogu/P1196.cpp
@@ -1,80 +1,48 @@
-#include <iostream>
 #include <algorithm>
-#include <cmath>
-#define MAXN 30005
+#include <iostream>
 using namespace std;
-int tops[MAXN];    //队首
-int rears[MAXN];   //队尾
-int ranks[MAXN];   //所在队伍排名
-int lengths[MAXN]; //队伍长度
-int pres[MAXN];    //上一个元素
-inline int find_top(int x)
+int g[30005], sz[30005], v[30005]; // v[x]:x到g[x]的距离
+int dfn(int x)
 {
-    if (tops[x] == x)
+    if (x != g[x])
     {
-        return x;
+        int f = g[x];
+        g[x] = dfn(g[x]);
+        v[x] += v[f];
     }
-    else
-    {
-        return tops[x] = find_top(tops[x]);
-    }
-}
-inline int find_rear(int x)
-{
-    if (rears[x] == x)
-    {
-        return x;
-    }
-    else
-    {
-        return rears[x] = find_rear(rears[x]);
-    }
+    return g[x];
 }
 int main()
 {
-    ios::sync_with_stdio(false);
-    for (int i = 1; i < MAXN; i++)
+    for (int i = 1; i <= 30000; i++)
     {
-        tops[i] = i;
-        rears[i] = i;
-        ranks[i] = 1;
-        lengths[i] = 1;
+        g[i] = i, sz[i] = 1;
     }
-    int t;
-    cin >> t;
-    for (int i = 1; i <= t; i++)
+    int p;
+    cin >> p;
+    while (p--)
     {
-        char type;
-        int origin, target;
-        cin >> type >> origin >> target;
-        int top1 = find_top(origin);
-        int top2 = find_top(target);
-        if (type == 'M') //合并
+        char opt;
+        int x, y;
+        cin >> opt >> x >> y;
+        int gx = dfn(x), gy = dfn(y);
+        if (opt == 'M')
         {
-            int rear1 = find_rear(origin);
-            int rear2 = find_rear(target);
-            tops[top1] = top2; //合并分组
-            rears[rear2] = rear1;
-            pres[top1] = rear2; //队伍1的队首接在队伍2的队尾
-            lengths[top2] += lengths[top1];
-            int tmp = 0; //重排编号
-            for (int j = rear1; j != rear2; j = pres[j])
-            {
-                ranks[j] = lengths[top2] - tmp;
-                tmp++;
-            }
+            v[gx] += sz[gy];
+            g[gx] = gy, sz[gy] += sz[gx];
         }
-        else //查询
+        else
         {
-            if (top1 != top2)
+            if (gx == gy)
             {
-                cout << -1 << '\n';
+                cout << abs(v[x] - v[y]) - 1 << '\n';
             }
             else
             {
-                cout << abs(ranks[origin] - ranks[target]) - 1 << '\n';
+                cout << "-1\n";
             }
         }
     }
     return 0;
 }
+// TAG: 权值并查集
