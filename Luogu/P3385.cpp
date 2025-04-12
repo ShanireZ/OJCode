6396cbc2418f5664e5800515b620a5be79eb2280
ShanireZ <shanire@skylinejoy.com>
2025-04-12 10:26:09 +0800

    Update

--- Luogu/P3385.cpp
+++ Luogu/P3385.cpp
@@ -1,72 +1,63 @@
-#include <cstring>
+#include <algorithm>
 #include <iostream>
 #include <queue>
+#include <vector>
 using namespace std;
-#define MX 6005
-int last[MX], pre[MX], to[MX], times[MX], inq[MX], d[MX], dis[MX];
-int n, m, epos;
-void addEdge(int u, int v, int w, int eid)
+struct Edge
 {
-    pre[eid] = last[u], to[eid] = v, d[eid] = w;
-    last[u] = eid;
-}
-bool SPFA()
-{
-    queue<int> q;
-    q.push(1), inq[1] = 1, dis[1] = 0;
-    while (q.size())
-    {
-        int now = q.front();
-        q.pop(), inq[now] = 0;
-        for (int i = last[now]; i; i = pre[i])
-        {
-            int nxt = to[i], w = d[i];
-            if (w + dis[now] < dis[nxt])
-            {
-                dis[nxt] = dis[now] + w;
-                if (inq[nxt] == 0)
-                {
-                    q.push(nxt), inq[nxt] = 1;
-                    if (++times[nxt] > n)
-                    {
-                        return true;
-                    }
-                }
-            }
-        }
-    }
-    return false;
-}
+    int v, w;
+};
+vector<Edge> to[2005];
+queue<int> q;
+int n, m, T, ok, dis[2005], inq[2005], l[2005];
 int main()
 {
-    int T;
     cin >> T;
     while (T--)
     {
-        memset(dis, 0x3f, sizeof(dis));
-        memset(last, 0, sizeof(last));
-        memset(times, 0, sizeof(times));
-        memset(inq, 0, sizeof(inq));
         cin >> n >> m;
-        epos = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            to[i].clear(), dis[i] = 0x3f3f3f3f;
+            l[i] = 0, inq[i] = 0;
+        }
         for (int i = 1; i <= m; i++)
         {
             int u, v, w;
             cin >> u >> v >> w;
-            addEdge(u, v, w, ++epos);
             if (w >= 0)
             {
-                addEdge(v, u, w, ++epos);
+                to[v].emplace_back(Edge{u, w});
+            }
+            to[u].emplace_back(Edge{v, w});
+        }
+        while (q.size())
+        {
+            q.pop();
+        }
+        ok = 1, dis[1] = 0, inq[1] = 1, q.emplace(1);
+        while (q.size() && ok)
+        {
+            int now = q.front();
+            inq[now] = 0, q.pop();
+            for (Edge e : to[now])
+            {
+                if (dis[e.v] > dis[now] + e.w)
+                {
+                    dis[e.v] = dis[now] + e.w, l[e.v] = l[now] + 1;
+                    if (l[e.v] == n)
+                    {
+                        ok = 0;
+                        break;
+                    }
+                    if (inq[e.v] == 0)
+                    {
+                        inq[e.v] = 1, q.emplace(e.v);
+                    }
+                }
             }
         }
-        if (SPFA())
-        {
-            cout << "YES" << endl;
-        }
-        else
-        {
-            cout << "NO" << endl;
-        }
+        cout << (ok ? "NO" : "YES") << endl;
     }
     return 0;
 }
