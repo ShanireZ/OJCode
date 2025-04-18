4e003a44a71cfc89bffa17791f4a6f0ede633021
ShanireZ <shanire@skylinejoy.com>
2025-04-18 15:37:34 +0800

    Update

--- Luogu/P1491.cpp
+++ Luogu/P1491.cpp
@@ -1,122 +1,83 @@
-#include <cstdio>
 #include <algorithm>
-#include <vector>
-#include <queue>
-#include <utility>
 #include <cmath>
+#include <iostream>
+#include <queue>
+#include <vector>
 using namespace std;
-int read();
+struct Node
+{
+    int x, y;
+};
+Node ns[205];
 struct Edge
 {
     int u, v;
     double w;
-    Edge(int U = 0, int V = 0, double W = 0)
-    {
-        u = U, v = V, w = W;
-    }
-    bool operator<(const Edge oth) const
+    bool operator<(const Edge &oth) const
     {
         return w > oth.w;
     }
 };
-struct Node
-{
-    int x, y, pre; //pre表示最短路的来源
-    double d;
-    vector<Edge> es;
-};
-Node ns[205];
-double cntdis(int a, int b)
-{
-    double ans = (ns[a].x - ns[b].x) * (ns[a].x - ns[b].x) + (ns[a].y - ns[b].y) * (ns[a].y - ns[b].y);
-    return sqrt(ans);
-}
+Edge es[10005];
 priority_queue<Edge> q;
-double dijk(int st, int n, int banf, int bant) //除去指定边的最短路
+vector<int> g[205], way;
+int n, m, rec[205];
+double dis[205], ans = 1e15;
+double dijkstra(int lmt)
 {
-    for (int i = 1; i <= n; i++)
-    {
-        ns[i].d = 1e15;
-    }
-    ns[st].d = 0;
-    for (int i = 0; i < ns[st].es.size(); i++)
-    {
-        q.push(ns[st].es[i]);
-    }
+    fill(dis + 1, dis + 1 + n, 1e15);
+    dis[1] = 0, q.emplace(Edge{1, 0, 0});
     while (q.size())
     {
-        Edge e = q.top();
+        int now = q.top().u;
+        double w = q.top().w;
         q.pop();
-        if (banf == e.u && bant == e.v || ns[e.v].d != 1e15)
+        if (w == dis[now])
         {
-            continue;
-        }
-        int now = e.v;
-        double w = e.w;
-        ns[now].d = w, ns[now].pre = e.u;
-        for (int i = 0; i < ns[now].es.size(); i++)
-        {
-            e = ns[now].es[i];
-            int nxt = e.v;
-            q.push(Edge(now, nxt, w + e.w));
+            for (int eid : g[now])
+            {
+                if (eid == lmt)
+                {
+                    continue;
+                }
+                int nxt = es[eid].u + es[eid].v - now;
+                double d = es[eid].w;
+                if (dis[nxt] > dis[now] + d)
+                {
+                    dis[nxt] = dis[now] + d, rec[nxt] = eid;
+                    q.emplace(Edge{nxt, 0, dis[nxt]});
+                }
+            }
         }
     }
-    return ns[n].d;
+    return dis[n];
 }
-vector<pair<int, int>> way; //最短路所有边
 int main()
 {
-    int n = read(), m = read();
+    scanf("%d%d", &n, &m);
     for (int i = 1; i <= n; i++)
     {
-        ns[i].x = read(), ns[i].y = read();
+        scanf("%d%d", &ns[i].x, &ns[i].y);
     }
     for (int i = 1; i <= m; i++)
     {
-        int u = read(), v = read();
-        double w = cntdis(u, v);
-        ns[u].es.push_back(Edge(u, v, w));
-        ns[v].es.push_back(Edge(v, u, w));
+        int a, b;
+        scanf("%d%d", &a, &b);
+        g[a].emplace_back(i), g[b].emplace_back(i);
+        double c = sqrt((ns[a].x - ns[b].x) * (ns[a].x - ns[b].x) + (ns[a].y - ns[b].y) * (ns[a].y - ns[b].y));
+        es[i] = Edge{a, b, c};
     }
-    dijk(1, n, 0, 0); //找到无限制的最短路
-    int t = n, f = ns[n].pre;
-    while (t != 1) //找到最短路的所有边
+    dijkstra(0);
+    int p = n;
+    while (p != 1)
     {
-        way.push_back(make_pair(f, t));
-        t = f, f = ns[t].pre;
+        way.emplace_back(rec[p]);
+        p = es[rec[p]].u + es[rec[p]].v - p;
     }
-    double ans = 1e16;
-    for (int i = 0; i < way.size(); i++) //每次去掉一条最短路边，再跑最短路
+    for (int lmt : way)
     {
-        ans = min(ans, dijk(1, n, way[i].first, way[i].second));
+        ans = min(ans, dijkstra(lmt));
     }
-    if (ans == 1e16)
-    {
-        printf("-1");
-    }
-    else
-    {
-        printf("%.2f", ans);
-    }
+    printf("%.2lf\n", ans == 1e15 ? -1 : ans);
     return 0;
-}
-int read()
-{
-    char ch = getchar();
-    while (ch != '-' && (ch > '9' || ch < '0'))
-    {
-        ch = getchar();
-    }
-    int t = 1, ans = 0;
-    if (ch == '-')
-    {
-        t = -1;
-        ch = getchar();
-    }
-    while (ch >= '0' && ch <= '9')
-    {
-        ans = ans * 10 + ch - '0';
-        ch = getchar();
-    }
-    return ans * t;
 }
