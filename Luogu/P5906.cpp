ba89c00b4a8641c433fe95b4e8e20b80e2aa136a
ShanireZ <shanire@skylinejoy.com>
2025-05-24 11:54:27 +0800

    Update

--- Luogu/P5906.cpp
+++ Luogu/P5906.cpp
@@ -1,103 +1,96 @@
 #include <algorithm>
 #include <cmath>
+#include <cstring>
 #include <iostream>
 using namespace std;
 #define MX 200005
-int ans[MX], a[MX], ls[MX], g[MX], fst[MX], lst[MX], rec[MX * 2], n, m, sz;
+int n, m, len, mpos, a[MX], ls[MX], fst[MX], lst[MX], ans[MX], rec[MX], bk[MX];
 struct Node
 {
-    int id, l, r;
+    int l, r, id;
+    bool operator<(const Node &oth) const
+    {
+        return bk[l] == bk[oth.l] ? r < oth.r : l < oth.l;
+    }
 };
 Node ns[MX];
-bool cmp(Node x, Node y)
-{
-    return (g[x.l] == g[y.l]) ? x.r < y.r : x.l < y.l;
-}
 int main()
 {
     cin >> n;
+    len = sqrt(n);
     for (int i = 1; i <= n; i++)
     {
         cin >> a[i];
-        ls[i] = a[i];
+        ls[i] = a[i], bk[i] = (i - 1) / len + 1;
     }
     sort(ls + 1, ls + 1 + n);
-    int len = unique(ls + 1, ls + 1 + n) - ls;
+    mpos = unique(ls + 1, ls + 1 + n) - ls;
     for (int i = 1; i <= n; i++)
     {
-        a[i] = lower_bound(ls + 1, ls + len, a[i]) - ls;
+        a[i] = lower_bound(ls + 1, ls + mpos, a[i]) - ls;
     }
     cin >> m;
-    sz = max(1, (int)sqrt(n));
-    for (int i = 1; i <= n; i++)
-    {
-        g[i] = (i - 1) / sz + 1;
-    }
     for (int i = 1; i <= m; i++)
     {
         cin >> ns[i].l >> ns[i].r;
         ns[i].id = i;
     }
-    sort(ns + 1, ns + 1 + m, cmp);
-    int now = 1;
-    while (now <= m)
+    sort(ns + 1, ns + 1 + m);
+    for (int i = 1; i <= m;)
     {
-        int res = 0, rpos = 0;
-        if (g[ns[now].l] == g[ns[now].r])
+        if (bk[ns[i].l] == bk[ns[i].r])
         {
-            for (int j = ns[now].l; j <= ns[now].r; j++)
+            int res = 0;
+            for (int j = ns[i].l; j <= ns[i].r; j++)
             {
-                if (fst[a[j]] == 0)
+                int x = a[j];
+                if (fst[x] == 0)
                 {
-                    fst[a[j]] = j, rec[++rpos] = a[j];
+                    fst[x] = j;
                 }
-                res = max(res, j - fst[a[j]]);
+                lst[x] = j, res = max(res, lst[x] - fst[x]);
             }
-            ans[ns[now].id] = res, now++;
+            for (int j = ns[i].l; j <= ns[i].r; j++)
+            {
+                int x = a[j];
+                fst[x] = lst[x] = 0;
+            }
+            ans[ns[i].id] = res, i++;
+            continue;
+        }
+        int l = bk[ns[i].l] * len + 1, r = l - 1, res = 0;
+        do
+        {
+            while (r < ns[i].r)
+            {
+                int x = a[++r];
+                if (fst[x] == 0)
+                {
+                    fst[x] = r;
+                }
+                lst[x] = r, res = max(res, lst[x] - fst[x]);
+            }
+            int nr = res, rpos = 0;
+            while (l > ns[i].l)
+            {
+                int x = a[--l];
+                if (lst[x] == 0)
+                {
+                    lst[x] = l, rec[++rpos] = x;
+                }
+                nr = max(nr, lst[x] - l);
+            }
             for (int j = 1; j <= rpos; j++)
             {
-                fst[rec[j]] = 0;
+                lst[rec[j]] = 0;
             }
-        }
-        else
-        {
-            int l = g[ns[now].l] * sz + 1, r = g[ns[now].l] * sz;
-            do
-            {
-                while (r < ns[now].r)
-                {
-                    r++;
-                    if (fst[a[r]] == 0)
-                    {
-                        fst[a[r]] = r, rec[++rpos] = a[r];
-                    }
-                    res = max(res, r - fst[a[r]]), lst[a[r]] = r;
-                }
-                int nr = res, bpos = rpos;
-                while (l > ns[now].l)
-                {
-                    l--;
-                    if (lst[a[l]] == 0)
-                    {
-                        lst[a[l]] = l, rec[++bpos] = a[l];
-                    }
-                    nr = max(nr, lst[a[l]] - l);
-                }
-                ans[ns[now].id] = nr, l = g[ns[now].l] * sz + 1, now++;
-                for (int j = rpos + 1; j <= bpos; j++)
-                {
-                    lst[rec[j]] = 0;
-                }
-            } while (now <= m && g[ns[now].l] == g[ns[now - 1].l]);
-            for (int j = 1; j <= rpos; j++)
-            {
-                fst[rec[j]] = lst[rec[j]] = 0;
-            }
-        }
+            ans[ns[i].id] = nr, l = bk[ns[i].l] * len + 1, i++;
+        } while (bk[ns[i - 1].l] == bk[ns[i].l]);
+        memset(fst, 0, sizeof(fst)), memset(lst, 0, sizeof(lst));
     }
     for (int i = 1; i <= m; i++)
     {
-        cout << ans[i] << "\n";
+        cout << ans[i] << endl;
     }
     return 0;
 }
