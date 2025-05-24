3e9fa5591c0fe3cd2e3c1ed423a1f6841cbda5e8
ShanireZ <shanire@skylinejoy.com>
2025-05-24 10:46:44 +0800

    Update

--- Luogu/P5906.cpp
+++ Luogu/P5906.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 using namespace std;
 #define MX 200005
-int ans[MX], a[MX], ls[MX], g[MX], fst[MX], lst[MX], rec[MX * 2];
+int ans[MX], a[MX], ls[MX], g[MX], fst[MX], lst[MX], rec[MX * 2], n, m, sz;
 struct Node
 {
     int id, l, r;
@@ -13,27 +13,13 @@ bool cmp(Node x, Node y)
 {
     return (g[x.l] == g[y.l]) ? x.r < y.r : x.l < y.l;
 }
-int read()
-{
-    int ans = 0;
-    char ch = getchar();
-    while (ch < '0' || ch > '9')
-    {
-        ch = getchar();
-    }
-    while (ch >= '0' && ch <= '9')
-    {
-        ans = ans * 10 + ch - '0';
-        ch = getchar();
-    }
-    return ans;
-}
 int main()
 {
-    int n = read();
+    cin >> n;
     for (int i = 1; i <= n; i++)
     {
-        ls[i] = a[i] = read();
+        cin >> a[i];
+        ls[i] = a[i];
     }
     sort(ls + 1, ls + 1 + n);
     int len = unique(ls + 1, ls + 1 + n) - ls;
@@ -41,23 +27,25 @@ int main()
     {
         a[i] = lower_bound(ls + 1, ls + len, a[i]) - ls;
     }
-    int m = read(), sz = max(1, int(n / sqrt(m)));
+    cin >> m;
+    sz = max(1, (int)sqrt(n));
     for (int i = 1; i <= n; i++)
     {
         g[i] = (i - 1) / sz + 1;
     }
     for (int i = 1; i <= m; i++)
     {
-        ns[i].l = read(), ns[i].r = read(), ns[i].id = i;
+        cin >> ns[i].l >> ns[i].r;
+        ns[i].id = i;
     }
     sort(ns + 1, ns + 1 + m, cmp);
-    int p = 1;
-    while (p <= m)
+    int now = 1;
+    while (now <= m)
     {
         int res = 0, rpos = 0;
-        if (g[ns[p].l] == g[ns[p].r])
+        if (g[ns[now].l] == g[ns[now].r])
         {
-            for (int j = ns[p].l; j <= ns[p].r; j++)
+            for (int j = ns[now].l; j <= ns[now].r; j++)
             {
                 if (fst[a[j]] == 0)
                 {
@@ -65,7 +53,7 @@ int main()
                 }
                 res = max(res, j - fst[a[j]]);
             }
-            ans[ns[p].id] = res, p++;
+            ans[ns[now].id] = res, now++;
             for (int j = 1; j <= rpos; j++)
             {
                 fst[rec[j]] = 0;
@@ -73,35 +61,34 @@ int main()
         }
         else
         {
-            int st = g[ns[p].l] * sz + 1, ed = g[ns[p].l] * sz;
+            int l = g[ns[now].l] * sz + 1, r = g[ns[now].l] * sz;
             do
             {
-                while (ed < ns[p].r)
+                while (r < ns[now].r)
                 {
-                    ed++;
-                    if (fst[a[ed]] == 0)
+                    r++;
+                    if (fst[a[r]] == 0)
                     {
-                        fst[a[ed]] = ed, rec[++rpos] = a[ed];
+                        fst[a[r]] = r, rec[++rpos] = a[r];
                     }
-                    res = max(res, ed - fst[a[ed]]), lst[a[ed]] = ed;
+                    res = max(res, r - fst[a[r]]), lst[a[r]] = r;
                 }
-                int nowr = res, nowp = rpos;
-                while (st > ns[p].l)
+                int nr = res, bpos = rpos;
+                while (l > ns[now].l)
                 {
-                    st--;
-                    if (lst[a[st]] == 0)
+                    l--;
+                    if (lst[a[l]] == 0)
                     {
-                        lst[a[st]] = st, rec[++nowp] = a[st];
+                        lst[a[l]] = l, rec[++bpos] = a[l];
                     }
-                    nowr = max(nowr, lst[a[st]] - st);
+                    nr = max(nr, lst[a[l]] - l);
                 }
-                ans[ns[p].id] = nowr, st = g[ns[p].l] * sz + 1, p++;
-                while (nowp > rpos)
+                ans[ns[now].id] = nr, l = g[ns[now].l] * sz + 1, now++;
+                for (int j = rpos + 1; j <= bpos; j++)
                 {
-                    lst[rec[nowp]] = 0;
-                    nowp--;
+                    lst[rec[j]] = 0;
                 }
-            } while (p <= m && g[ns[p].l] == g[ns[p - 1].l]);
+            } while (now <= m && g[ns[now].l] == g[ns[now - 1].l]);
             for (int j = 1; j <= rpos; j++)
             {
                 fst[rec[j]] = lst[rec[j]] = 0;
@@ -110,8 +97,7 @@ int main()
     }
     for (int i = 1; i <= m; i++)
     {
-        printf("%d\n", ans[i]);
+        cout << ans[i] << "\n";
     }
     return 0;
 }
-// TAG: 回滚莫队
