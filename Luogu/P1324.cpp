262aee9c27e8d78f9290470691c901d6a702b966
ShanireZ <shanire@skylinejoy.com>
2025-06-15 09:42:17 +0800

    Update

---
+++ Luogu/P1324.cpp
@@ -0,0 +1,31 @@
+#include <algorithm>
+#include <iostream>
+using namespace std;
+int n, m, h[2005], s[2005];
+int main()
+{
+    cin >> n >> m;
+    for (int i = 1; i < n; i++)
+    {
+        cin >> h[i];
+    }
+    for (int i = 1; i < m; i++)
+    {
+        cin >> s[i];
+    }
+    sort(h + 1, h + n, greater<int>()), sort(s + 1, s + m, greater<int>());
+    long long ans = 0, ph = 1, ps = 1;
+    while (ph < n || ps < m)
+    {
+        if (ps == m || (ph < n && h[ph] > s[ps]))
+        {
+            ans += h[ph++] * ps;
+        }
+        else
+        {
+            ans += s[ps++] * ph;
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
