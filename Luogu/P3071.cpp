33231f4210e88ac6d5ea85dc356728ae20c343cc
ShanireZ <shanire@skylinejoy.com>
2025-07-15 16:14:01 +0800

    Update

--- Luogu/P3071.cpp
+++ Luogu/P3071.cpp
@@ -6,17 +6,18 @@ struct Node
     int lc, rc, v, lv, rv, tag;
 };
 Node ns[1000005];
-int n, m, root, pos, cnt;
+int root, pos, n, m, ans;
 void update(int now, int l, int r)
 {
     int mid = (l + r) / 2;
     ns[now].v = max(ns[ns[now].lc].v, ns[ns[now].rc].v);
-    ns[now].v = max(ns[ns[now].lc].rv + ns[ns[now].rc].lv, ns[now].v);
-    ns[now].lv = ns[ns[now].lc].lv, ns[now].rv = ns[ns[now].rc].rv;
+    ns[now].v = max(ns[now].v, ns[ns[now].lc].rv + ns[ns[now].rc].lv);
+    ns[now].lv = ns[ns[now].lc].lv;
     if (ns[ns[now].lc].lv == mid - l + 1)
     {
         ns[now].lv += ns[ns[now].rc].lv;
     }
+    ns[now].rv = ns[ns[now].rc].rv;
     if (ns[ns[now].rc].rv == r - mid)
     {
         ns[now].rv += ns[ns[now].lc].rv;
@@ -38,85 +39,67 @@ void maketree(int &now, int l, int r)
 void pushdown(int now, int l, int r)
 {
     int mid = (l + r) / 2;
-    if (ns[now].tag == -1)
-    {
-        ns[ns[now].lc].tag = ns[ns[now].rc].tag = -1;
-        ns[ns[now].lc].v = ns[ns[now].lc].lv = ns[ns[now].lc].rv = mid - l + 1;
-        ns[ns[now].rc].v = ns[ns[now].rc].lv = ns[ns[now].rc].rv = r - mid;
-    }
-    else if (ns[now].tag == 1)
+    if (ns[now].tag == 1)
     {
         ns[ns[now].lc].tag = ns[ns[now].rc].tag = 1;
         ns[ns[now].lc].v = ns[ns[now].lc].lv = ns[ns[now].lc].rv = 0;
         ns[ns[now].rc].v = ns[ns[now].rc].lv = ns[ns[now].rc].rv = 0;
     }
+    else if (ns[now].tag == -1)
+    {
+        ns[ns[now].lc].tag = ns[ns[now].rc].tag = -1;
+        ns[ns[now].lc].v = ns[ns[now].lc].lv = ns[ns[now].lc].rv = mid - l + 1;
+        ns[ns[now].rc].v = ns[ns[now].rc].lv = ns[ns[now].rc].rv = r - mid;
+    }
     ns[now].tag = 0;
 }
-void cl(int now, int l, int r, int x, int y)
+void edit(int now, int l, int r, int x, int y, int k)
 {
     if (x <= l && y >= r)
     {
-        ns[now].tag = -1;
-        ns[now].v = ns[now].lv = ns[now].rv = r - l + 1;
+        ns[now].tag = k;
+        if (k == 1)
+        {
+            ns[now].v = ns[now].lv = ns[now].rv = 0;
+        }
+        else
+        {
+            ns[now].v = ns[now].lv = ns[now].rv = r - l + 1;
+        }
         return;
     }
     pushdown(now, l, r);
     int mid = (l + r) / 2;
     if (x <= mid)
     {
-        cl(ns[now].lc, l, mid, x, y);
+        edit(ns[now].lc, l, mid, x, y, k);
     }
     if (y > mid)
     {
-        cl(ns[now].rc, mid + 1, r, x, y);
+        edit(ns[now].rc, mid + 1, r, x, y, k);
     }
     update(now, l, r);
 }
-void editsuf(int now, int l, int r, int x, int y)
+int query(int now, int l, int r, int x)
 {
-    if (x <= l && y >= r)
+    if (ns[now].lv >= x)
     {
-        ns[now].tag = 1;
-        ns[now].v = ns[now].lv = ns[now].rv = 0;
-        return;
+        return l;
     }
     pushdown(now, l, r);
     int mid = (l + r) / 2;
-    if (x <= mid)
+    if (ns[ns[now].lc].v >= x)
     {
-        editsuf(ns[now].lc, l, mid, x, y);
+        return query(ns[now].lc, l, mid, x);
     }
-    if (y > mid)
+    else if (ns[ns[now].lc].rv + ns[ns[now].rc].lv >= x)
     {
-        editsuf(ns[now].rc, mid + 1, r, x, y);
-    }
-    update(now, l, r);
-}
-void edit(int now, int l, int r, int ex)
-{
-    if (r - l + 1 == ex && ns[now].lv == ex)
-    {
-        ns[now].tag = 1;
-        ns[now].v = ns[now].lv = ns[now].rv = 0;
-        return;
-    }
-    pushdown(now, l, r);
-    int mid = (l + r) / 2;
-    if (ns[ns[now].lc].v >= ex)
-    {
-        edit(ns[now].lc, l, mid, ex);
-    }
-    else if (ns[ns[now].lc].rv && ns[ns[now].lc].rv + ns[ns[now].rc].lv >= ex)
-    {
-        int x = ns[ns[now].lc].rv;
-        editsuf(ns[now].lc, l, mid, mid - x + 1, mid);
-        edit(ns[now].rc, mid + 1, r, ex - x);
+        return mid - ns[ns[now].lc].rv + 1;
     }
     else
     {
-        edit(ns[now].rc, mid + 1, r, ex);
+        return query(ns[now].rc, mid + 1, r, x);
     }
-    update(now, l, r);
 }
 int main()
 {
@@ -125,23 +108,24 @@ int main()
     while (m--)
     {
         char opt;
-        int x, y;
-        cin >> opt >> x;
+        int a, b;
+        cin >> opt >> a;
         if (opt == 'A')
         {
-            if (ns[root].v < x)
+            if (ns[root].v < a)
             {
-                cnt++;
+                ans++;
                 continue;
             }
-            edit(root, 1, n, x);
+            int p = query(root, 1, n, a);
+            edit(root, 1, n, p, p + a - 1, 1);
         }
         else
         {
-            cin >> y;
-            cl(root, 1, n, x, y);
+            cin >> b;
+            edit(root, 1, n, a, b, -1);
         }
     }
-    cout << cnt << endl;
+    cout << ans << endl;
     return 0;
 }
