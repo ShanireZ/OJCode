0ef5282b3a635f894b0a6e627c4fa3a075b3add4
ShanireZ <shanire@skylinejoy.com>
2025-07-15 14:41:47 +0800

    Update

--- Luogu/P2894.cpp
+++ Luogu/P2894.cpp
@@ -1,156 +1,132 @@
-#include <cstdio>
 #include <algorithm>
+#include <iostream>
 using namespace std;
-int read();
 struct Node
 {
-	int lc, rc, len, mv, lmv, rmv, lzy; //lzy 0无事 1开房 2退房
+    int lc, rc, v, lv, rv, tag;
 };
-Node ns[200005];
-int root, pos;
-void pushdown(int now)
+Node ns[100005];
+int root, pos, n, m;
+void update(int now, int l, int r)
 {
-	if (ns[now].lzy == 0)
-	{
-		return;
-	}
-	int lc = ns[now].lc, rc = ns[now].rc;
-	if (ns[now].lzy == 1) // 开房标记
-	{
-		ns[lc].mv = ns[lc].lmv = ns[lc].rmv = 0;
-		ns[rc].mv = ns[rc].lmv = ns[rc].rmv = 0;
-	}
-	else //退房标记
-	{
-		ns[lc].mv = ns[lc].lmv = ns[lc].rmv = ns[lc].len;
-		ns[rc].mv = ns[rc].lmv = ns[rc].rmv = ns[rc].len;
-	}
-	ns[ns[now].lc].lzy = ns[ns[now].rc].lzy = ns[now].lzy;
-	ns[now].lzy = 0;
+    int mid = (l + r) / 2;
+    ns[now].v = max(ns[ns[now].lc].v, ns[ns[now].rc].v);
+    ns[now].v = max(ns[now].v, ns[ns[now].lc].rv + ns[ns[now].rc].lv);
+    ns[now].lv = ns[ns[now].lc].lv;
+    if (ns[ns[now].lc].lv == mid - l + 1)
+    {
+        ns[now].lv += ns[ns[now].rc].lv;
+    }
+    ns[now].rv = ns[ns[now].rc].rv;
+    if (ns[ns[now].rc].rv == r - mid)
+    {
+        ns[now].rv += ns[ns[now].lc].rv;
+    }
 }
-void update(int now)
+void maketree(int &now, int l, int r)
 {
-	int lc = ns[now].lc, rc = ns[now].rc;
-	ns[now].len = ns[lc].len + ns[rc].len;
-	ns[now].lmv = ns[lc].lmv;
-	if (ns[lc].len == ns[lc].lmv)
-	{
-		ns[now].lmv += ns[rc].lmv;
-	}
-	ns[now].rmv = ns[rc].rmv;
-	if (ns[rc].len == ns[rc].rmv)
-	{
-		ns[now].rmv += ns[lc].rmv;
-	}
-	ns[now].mv = max(max(ns[lc].mv, ns[rc].mv), ns[lc].rmv + ns[rc].lmv);
+    now = ++pos;
+    if (l == r)
+    {
+        ns[now].v = ns[now].lv = ns[now].rv = 1;
+        return;
+    }
+    int mid = (l + r) / 2;
+    maketree(ns[now].lc, l, mid);
+    maketree(ns[now].rc, mid + 1, r);
+    update(now, l, r);
 }
-void newTree(int &now, int l, int r)
+void pushdown(int now, int l, int r)
 {
-	now = ++pos;
-	if (l == r)
-	{
-		ns[now].len = ns[now].mv = ns[now].lmv = ns[now].rmv = 1;
-		return;
-	}
-	int mid = (l + r) / 2;
-	newTree(ns[now].lc, l, mid);
-	newTree(ns[now].rc, mid + 1, r);
-	update(now);
+    int mid = (l + r) / 2;
+    if (ns[now].tag == 1)
+    {
+        ns[ns[now].lc].tag = ns[ns[now].rc].tag = 1;
+        ns[ns[now].lc].v = ns[ns[now].lc].lv = ns[ns[now].lc].rv = 0;
+        ns[ns[now].rc].v = ns[ns[now].rc].lv = ns[ns[now].rc].rv = 0;
+    }
+    else if (ns[now].tag == -1)
+    {
+        ns[ns[now].lc].tag = ns[ns[now].rc].tag = -1;
+        ns[ns[now].lc].v = ns[ns[now].lc].lv = ns[ns[now].lc].rv = mid - l + 1;
+        ns[ns[now].rc].v = ns[ns[now].rc].lv = ns[ns[now].rc].rv = r - mid;
+    }
+    ns[now].tag = 0;
 }
-void edit(int now, int l, int r, int a, int b, int k)
+void edit(int now, int l, int r, int x, int y, int k)
 {
-	pushdown(now);
-	if (a <= l && r <= b)
-	{
-		if (k == 1)
-		{
-			ns[now].mv = ns[now].lmv = ns[now].rmv = 0;
-		}
-		else
-		{
-			ns[now].mv = ns[now].lmv = ns[now].rmv = ns[now].len;
-		}
-		ns[now].lzy = k;
-		return;
-	}
-	int mid = (l + r) / 2;
-	if (a <= mid && b >= l)
-	{
-		edit(ns[now].lc, l, mid, a, b, k);
-	}
-	if (a <= r && b >= mid + 1)
-	{
-		edit(ns[now].rc, mid + 1, r, a, b, k);
-	}
-	update(now);
+    if (x <= l && y >= r)
+    {
+        ns[now].tag = k;
+        if (k == 1)
+        {
+            ns[now].v = ns[now].lv = ns[now].rv = 0;
+        }
+        else
+        {
+            ns[now].v = ns[now].lv = ns[now].rv = r - l + 1;
+        }
+        return;
+    }
+    pushdown(now, l, r);
+    int mid = (l + r) / 2;
+    if (x <= mid)
+    {
+        edit(ns[now].lc, l, mid, x, y, k);
+    }
+    if (y > mid)
+    {
+        edit(ns[now].rc, mid + 1, r, x, y, k);
+    }
+    update(now, l, r);
 }
-int querry(int now, int l, int r, int a)
+int query(int now, int l, int r, int x)
 {
-	pushdown(now);
-	if (l == r)
-	{
-		return l;
-	}
-	int mid = (l + r) / 2;
-	if (ns[ns[now].lc].mv >= a)
-	{
-		return querry(ns[now].lc, l, mid, a);
-	}
-	else if (ns[ns[now].lc].rmv + ns[ns[now].rc].lmv >= a)
-	{
-		return mid - ns[ns[now].lc].rmv + 1;
-	}
-	else
-	{
-		return querry(ns[now].rc, mid + 1, r, a);
-	}
+    if (ns[now].lv >= x)
+    {
+        return l;
+    }
+    pushdown(now, l, r);
+    int mid = (l + r) / 2;
+    if (ns[ns[now].lc].v >= x)
+    {
+        return query(ns[now].lc, l, mid, x);
+    }
+    else if (ns[ns[now].lc].rv + ns[ns[now].rc].lv >= x)
+    {
+        return mid - ns[ns[now].lc].rv + 1;
+    }
+    else if (ns[ns[now].rc].v >= x)
+    {
+        return query(ns[now].rc, mid + 1, r, x);
+    }
+    else
+    {
+        return 0;
+    }
 }
 int main()
 {
-	// freopen("xxx.in", "r", stdin);
-	// freopen("xxx.out", "w", stdout);
-	int n = read(), m = read();
-	newTree(root, 1, n);
-	for (int i = 1; i <= m; i++)
-	{
-		int t = read();
-		if (t == 1)
-		{
-			int a = read();
-			if (a > ns[root].mv)
-			{
-				printf("0\n");
-				continue;
-			}
-			int p = querry(root, 1, n, a);
-			printf("%d\n", p);
-			edit(root, 1, n, p, p + a - 1, 1);
-		}
-		else
-		{
-			int a = read(), b = read();
-			edit(root, 1, n, a, a + b - 1, 2);
-		}
-	}
-	return 0;
-}
-int read()
-{
-	char ch = getchar();
-	while (ch != '-' && (ch > '9' || ch < '0'))
-	{
-		ch = getchar();
-	}
-	int t = 1, ans = 0;
-	if (ch == '-')
-	{
-		t = -1;
-		ch = getchar();
-	}
-	while (ch >= '0' && ch <= '9')
-	{
-		ans = ans * 10 + ch - '0';
-		ch = getchar();
-	}
-	return t * ans;
+    cin >> n >> m;
+    maketree(root, 1, n);
+    while (m--)
+    {
+        int opt, x, y;
+        cin >> opt >> x;
+        if (opt == 1)
+        {
+            int p = query(root, 1, n, x);
+            cout << p << endl;
+            if (p != 0)
+            {
+                edit(root, 1, n, p,  p + x - 1, 1);
+            }
+        }
+        else
+        {
+            cin >> y;
+            edit(root, 1, n, x, x + y - 1, -1);
+        }
+    }
+    return 0;
 }
