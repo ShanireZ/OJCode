fde8fa033897ee17a7f432bab0b8f7cbaee87d6f
Shanire <shanire@skylinejoy.com>
2025-10-03 09:50:02 +0800

    Update

--- Luogu/P4782.cpp
+++ Luogu/P4782.cpp
@@ -1,84 +1,82 @@
 #include <algorithm>
 #include <iostream>
+#include <stack>
 #include <vector>
 using namespace std;
 #define MX 2000005
-struct Node
-{
-    int low, dfn, g;
-};
-Node ns[MX];
-vector<int> to[MX]; // 1~n 真 n+1~2n 假
-int read(), pos, gpos, spos, s[MX];
+vector<int> to[MX];
+stack<int> s;
+int dfn[MX], low[MX], g[MX], n, m, npos, gpos;
 void tarjan(int now)
 {
-    ns[now].dfn = ns[now].low = ++pos;
-    s[++spos] = now;
-    for (int nxt : to[now])
-    {
-        if (ns[nxt].dfn == 0)
-        {
-            tarjan(nxt);
-            ns[now].low = min(ns[now].low, ns[nxt].low);
-        }
-        else if (ns[nxt].g == 0)
-        {
-            ns[now].low = min(ns[now].low, ns[nxt].dfn);
-        }
-    }
-    if (ns[now].dfn == ns[now].low)
-    {
-        gpos++;
-        while (now != s[spos])
-        {
-            ns[s[spos--]].g = gpos;
-        }
-        ns[s[spos--]].g = gpos;
-    }
+	dfn[now] = low[now] = ++npos, s.push(now);
+	for (int nxt : to[now])
+	{
+		if (dfn[nxt] == 0)
+		{
+			tarjan(nxt);
+			low[now] = min(low[now], low[nxt]);
+		}
+		else if (g[nxt] == 0)
+		{
+			low[now] = min(low[now], dfn[nxt]);
+		}
+	}
+	if (dfn[now] == low[now])
+	{
+		++gpos;
+		while (s.top() != now)
+		{
+			g[s.top()] = gpos, s.pop();
+		}
+		g[now] = gpos, s.pop();
+	}
 }
 int main()
 {
-    int n = read(), m = read();
-    for (int i = 1; i <= m; i++)
-    {
-        int x = read(), a = read(), y = read(), b = read();
-        to[x + n * a].push_back(y + n * !b);
-        to[y + n * b].push_back(x + n * !a);
-    }
-    for (int i = 1; i <= n * 2; i++)
-    {
-        if (ns[i].dfn == 0)
-        {
-            tarjan(i);
-        }
-    }
-    for (int i = 1; i <= n; i++)
-    {
-        if (ns[i].g == ns[i + n].g)
-        {
-            cout << "IMPOSSIBLE\n";
-            return 0;
-        }
-    }
-    cout << "POSSIBLE\n";
-    for (int i = 1; i <= n; i++)
-    {
-        cout << (ns[i].g < ns[i + n].g) << " ";
-    }
-    return 0;
-}
-int read()
-{
-    int ans = 0;
-    char ch = getchar();
-    while (ch < '0' || ch > '9')
-    {
-        ch = getchar();
-    }
-    while (ch >= '0' && ch <= '9')
-    {
-        ans = ans * 10 + ch - '0';
-        ch = getchar();
-    }
-    return ans;
+	ios::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+	cin >> n >> m;
+	for (int i = 1; i <= m; i++)
+	{
+		int x, a, y, b;
+		cin >> x >> a >> y >> b;
+		to[x + n * a].push_back(y + n * !b);
+		to[y + n * b].push_back(x + n * !a);
+		// if (a == 1)
+		// {
+		// 	b == 1 ? to[x + n].push_back(y) : to[x + n].push_back(y + n);
+		// }
+		// else
+		// {
+		// 	b == 1 ? to[x].push_back(y) : to[x].push_back(y + n);
+		// }
+		// if (b == 1)
+		// {
+		// 	a == 1 ? to[y + n].push_back(x) : to[y + n].push_back(x + n);
+		// }
+		// else
+		// {
+		// 	a == 1 ? to[y].push_back(x) : to[y].push_back(x + n);
+		// }
+	}
+	for (int i = 1; i <= n + n; i++)
+	{
+		if (dfn[i] == 0)
+		{
+			tarjan(i);
+		}
+		if (i > n && g[i] == g[i - n])
+		{
+			cout << "IMPOSSIBLE" << endl;
+			return 0;
+		}
+	}
+	cout << "POSSIBLE" << endl;
+	for (int i = 1; i <= n; i++)
+	{
+		cout << (g[i] < g[n + i] ? 1 : 0) << " "; // 输出更上游的结果,其限制更少
+	}
+	cout << endl;
+	return 0;
 }
