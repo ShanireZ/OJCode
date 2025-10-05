f20e79c099757b79173af589ed43c37d7eefc523
Shanire <shanire@skylinejoy.com>
2025-10-05 10:28:19 +0800

    Update

--- Luogu/P3376 EK.cpp
+++ Luogu/P3376 EK.cpp
@@ -1,73 +1,69 @@
 #include <algorithm>
-#include <cstring>
 #include <iostream>
 #include <queue>
 #include <vector>
 using namespace std;
-struct Edge
-{
-    long long to, w;
-};
-Edge es[10005];
-int flag[205][205], vis[205], from[205], n, m, s, t;
-long long flow[205];
 vector<int> to[205];
-queue<int> q;
-long long bfs()
+queue<pair<int, long long>> q;
+long long n, m, s, t, ans, vis[205], f[205][205], rd[205][205], pre[205];
+long long bfs(int st)
 {
-    memset(vis, 0, sizeof(vis)), memset(flow, 0, sizeof(flow)); // flow[t] = 0
-    queue<int> tmp;
-    swap(tmp, q);
-    q.push(s), vis[s] = 1, flow[s] = 1e10;
-    while (q.size())
-    {
-        int now = q.front();
-        q.pop();
-        for (int eid : to[now])
-        {
-            long long nxt = es[eid].to, cap = es[eid].w;
-            if (vis[nxt] || cap == 0)
-            {
-                continue;
-            }
-            q.push(nxt), vis[nxt] = 1, flow[nxt] = min(flow[now], cap), from[nxt] = now;
-            if (nxt == t)
-            {
-                goto pos;
-            }
-        }
-    }
-pos:
-    int now = from[t], nxt = t;
-    while (now && flow[t])
-    {
-        int eid = flag[now][nxt];
-        es[eid].w -= flow[t];
-        (eid <= m) ? (es[eid + m].w += flow[t]) : (es[eid - m].w += flow[t]);
-        nxt = now, now = from[now];
-    }
-    return flow[t];
+	long long ex = 0;
+	q.push({st, 1e15}), vis[st] = 1;
+	while (q.size())
+	{
+		long long now = q.front().first, flow = q.front().second;
+		q.pop();
+		if (now == t)
+		{
+			ex = flow;
+			break;
+		}
+		for (int nxt : to[now])
+		{
+			long long cap = min(flow, f[now][nxt]);
+			if (vis[nxt] == 0 && cap)
+			{
+				q.push({nxt, cap}), vis[nxt] = 1, pre[nxt] = now;
+			}
+		}
+	}
+	int now = pre[t], nxt = t;
+	while (now && ex)
+	{
+		f[now][nxt] -= ex, f[nxt][now] += ex;
+		nxt = now, now = pre[now];
+	}
+	return ex;
 }
 int main()
 {
-    cin >> n >> m >> s >> t;
-    for (int i = 1; i <= m; i++)
-    {
-        long long u, v, w;
-        cin >> u >> v >> w;
-        if (flag[u][v] == 0)
-        {
-            flag[u][v] = i, to[u].push_back(i), es[i].to = v;
-            flag[v][u] = i + m, to[v].push_back(i + m), es[i + m].to = u;
-        }
-        es[flag[u][v]].w += w;
-    }
-    long long ans = 0, add = bfs();
-    while (add)
-    {
-        ans += add;
-        add = bfs();
-    }
-    cout << ans << "\n";
-    return 0;
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cin >> n >> m >> s >> t;
+	for (int i = 1; i <= m; i++)
+	{
+		int u, v, w;
+		cin >> u >> v >> w;
+		if (rd[u][v] == 0 && w)
+		{
+			to[u].push_back(v), rd[u][v] = 1;
+			to[v].push_back(u), rd[v][u] = 1;
+		}
+		f[u][v] += w;
+	}
+	long long ex = bfs(s);
+	while (ex)
+	{
+		ans += ex;
+		fill(vis, vis + n + 1, 0);
+		fill(pre, pre + n + 1, 0);
+		while(q.size())
+		{
+			q.pop();
+		}
+		ex = bfs(s);
+	}
+	cout << ans << endl;
+	return 0;
 }
