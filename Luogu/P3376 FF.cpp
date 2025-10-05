dde56fb13fed8a16e3e1ce96ccb4ebadecc10a07
Shanire <shanire@skylinejoy.com>
2025-10-05 10:15:18 +0800

    Update

--- Luogu/P3376 FF.cpp
+++ Luogu/P3376 FF.cpp
@@ -1,60 +1,55 @@
 #include <algorithm>
-#include <cstring>
 #include <iostream>
 #include <vector>
 using namespace std;
-struct Edge
-{
-    long long to, w;
-};
-Edge es[10005];
-int flag[205][205], vis[205], n, m, s, t;
 vector<int> to[205];
+long long n, m, s, t, ans, vis[205], f[205][205], rd[205][205];
 long long dfs(int now, long long flow)
 {
-    if (now == t)
-    {
-        return flow;
-    }
-    vis[now] = 1;
-    for (int eid : to[now])
-    {
-        long long nxt = es[eid].to, cap = es[eid].w;
-        if (vis[nxt] == 1 || cap == 0)
-        {
-            continue;
-        }
-        long long add = dfs(nxt, min(flow, cap));
-        if (add)
-        {
-            es[eid].w -= add;
-            (eid <= m) ? (es[eid + m].w += add) : (es[eid - m].w += add);
-            return add;
-        }
-    }
-    return 0;
+	if (now == t)
+	{
+		return flow;
+	}
+	vis[now] = 1;
+	long long ex = 0;
+	for (int nxt : to[now])
+	{
+		long long cap = min(f[now][nxt], flow);
+		if (vis[nxt] == 0 && cap)
+		{
+			ex = dfs(nxt, cap);
+			f[now][nxt] -= ex, f[nxt][now] += ex;
+			if (ex)
+			{
+				break;
+			}
+		}
+	}
+	return ex;
 }
 int main()
 {
-    cin >> n >> m >> s >> t;
-    for (int i = 1; i <= m; i++)
-    {
-        long long u, v, w;
-        cin >> u >> v >> w;
-        if (flag[u][v] == 0)
-        {
-            flag[u][v] = i, to[u].push_back(i), es[i].to = v;
-            flag[v][u] = i + m, to[v].push_back(i + m), es[i + m].to = u;
-        }
-        es[flag[u][v]].w += w;
-    }
-    long long ans = 0, add = dfs(s, 1e10);
-    while (add)
-    {
-        ans += add;
-        memset(vis, 0, sizeof(vis));
-        add = dfs(s, 1e10);
-    }
-    cout << ans << "\n";
-    return 0;
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cin >> n >> m >> s >> t;
+	for (int i = 1; i <= m; i++)
+	{
+		int u, v, w;
+		cin >> u >> v >> w;
+		if (rd[u][v] == 0 && w)
+		{
+			to[u].push_back(v), rd[u][v] = 1;
+			to[v].push_back(u), rd[v][u] = 1;
+		}
+		f[u][v] += w;
+	}
+	long long ex = dfs(s, 1e15);
+	while (ex)
+	{
+		ans += ex;
+		fill(vis, vis + n + 1, 0);
+		ex = dfs(s, 1e15);
+	}
+	cout << ans << endl;
+	return 0;
 }
