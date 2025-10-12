bf3ac9f4fa31d7b0547fd680d56bf265655dbf64
Shanire <shanire@skylinejoy.com>
2025-10-12 11:11:13 +0800

    Update

---
+++ Luogu/P13014.cpp
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <iostream>
+using namespace std;
+int a[100005], n, q;
+int main()
+{
+    cin >> n >> q;
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> a[i];
+    }
+    int t = a[2] - a[1];
+    for (int i = 3; i <= n; i++)
+    {
+        t = __gcd(t, a[i] - a[1]);
+    }
+    for (int i = 1; i <= q; i++)
+    {
+        int res = __gcd(t, a[1] + i);
+        cout << res << endl;
+    }
+    return 0;
+}
