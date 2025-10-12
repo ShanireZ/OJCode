1bcf69c25646a76c4e7f4f2048a8b219a0254e6f
ShanireZ <shanire@skylinejoy.com>
2025-10-12 15:36:09 +0800

    Update

---
+++ Luogu/P14073.cpp
@@ -0,0 +1,21 @@
+#include <algorithm>
+#include <iostream>
+using namespace std;
+int n, ans, ok[100005];
+int main()
+{
+	cin >> n;
+	for (int i = 2; i <= n; i++)
+	{
+		if (ok[i] == 0)
+		{
+			for (int j = 2; i * j <= n; j++)
+			{
+				ok[i * j] = 1;
+			}
+			ans++;
+		}
+	}
+	cout << ans + 1 << endl;
+	return 0;
+}
