a0a52850d5e19dd2da497c3d7dee2729d11e5500
Shanire <shanire@skylinejoy.com>
2025-11-19 13:47:11 +0800

    Update

---
+++ SP UVA AT/ABC432A.cpp
@@ -0,0 +1,22 @@
+#include <algorithm>
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a, b, c;
+	cin >> a >> b >> c;
+	if (a < b)
+	{
+		swap(a, b);
+	}
+	if (a < c)
+	{
+		swap(a, c);
+	}
+	if (b < c)
+	{
+		swap(b, c);
+	}
+	cout << a << b << c << endl;
+	return 0;
+}
