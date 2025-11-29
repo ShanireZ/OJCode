d6d13be3987100a6ddfe6cb44daef270d89626df
Shanire <shanire@skylinejoy.com>
2025-11-29 09:38:35 +0800

    Update

--- Luogu/B4071.cpp
+++ Luogu/B4071.cpp
@@ -1,51 +1,44 @@
 #include <algorithm>
 #include <iostream>
-#include <set>
+#include <queue>
 using namespace std;
-multiset<int> cl[1005], all;
+vector<long long> cl[1005], kc;
+long long n, m, ans = 1e18;
 int main()
 {
-    int n, m;
-    cin >> n >> m;
-    for (int i = 1; i <= m; i++)
-    {
-        int p, c;
-        cin >> p >> c;
-        cl[p].emplace(c);
-    }
-    long long ans = 1e18;
-    for (int tar = cl[1].size(); tar <= m; tar++)
-    {
-        all.clear();
-        long long res = 0, cnt = cl[1].size();
-        for (int i = 2; i <= n; i++)
-        {
-            int dlt = cl[i].size() - (tar - 1);
-            for (auto x : cl[i])
-            {
-                if (dlt > 0)
-                {
-                    res += x, cnt++, dlt--;
-                }
-                else
-                {
-                    all.emplace(x);
-                }
-            }
-        }
-        for (auto x : all)
-        {
-            if (cnt < tar)
-            {
-                res += x, cnt++;
-            }
-            else
-            {
-                break;
-            }
-        }
-        ans = min(ans, res);
-    }
-    cout << ans << endl;
-    return 0;
+	cin >> n >> m;
+	for (int i = 1; i <= m; i++)
+	{
+		long long p, c;
+		cin >> p >> c;
+		cl[p].push_back(c);
+	}
+	for (int i = 1; i <= n; i++)
+	{
+		sort(cl[i].begin(), cl[i].end());
+	}
+	for (int tar = cl[1].size(); tar <= m; tar++)
+	{
+		long long cost = 0, cnt = cl[1].size();
+		for (int j = 2; j <= n; j++)
+		{
+			long long sz = cl[j].size(), pos = 0;
+			while (sz >= tar && pos < (int)cl[j].size())
+			{
+				cost += cl[j][pos++], sz--, cnt++;
+			}
+			while (pos < (int)cl[j].size())
+			{
+				kc.push_back(cl[j][pos++]);
+			}
+		}
+		sort(kc.begin(), kc.end());
+		for (int j = 0; j < (int)kc.size() && cnt < tar; j++)
+		{
+			cost += kc[j], cnt++;
+		}
+		ans = min(ans, cost), kc.clear();
+	}
+	cout << ans << endl;
+	return 0;
 }
