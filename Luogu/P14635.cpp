c9f51a0c1a41d922b2fbf80fe04d1fb7923d2c12
Shanire <shanire@skylinejoy.com>
2025-11-29 15:08:35 +0800

    Update

--- Luogu/P14635.cpp
+++ Luogu/P14635.cpp
@@ -1,38 +1,23 @@
 #include <algorithm>
 #include <iostream>
-#include <vector>
 using namespace std;
-long long n, m, mn = 2e18, x[100005], y[100005], pre[100005];
-bool check(long long k)
-{
-    for (__int128 solo = k % 2, ps = k / 2; solo <= min(n, k); solo += 2, ps--)
-    {
-        if (ps * mn + pre[solo] <= m)
-        {
-            return true;
-        }
-    }
-    return false;
-};
+long long n, m, ans, mn = 2e18, x[100005], y;
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
     cin >> n >> m;
     for (int i = 1; i <= n; ++i)
     {
-        cin >> x[i] >> y[i];
-        mn = min(mn, x[i] + y[i]);
+        cin >> x[i] >> y;
+        mn = min(mn, x[i] + y);
     }
     sort(x + 1, x + n + 1);
-    for (int i = 1; i <= n; ++i)
+    for (int i = 0; i <= n && m >= x[i]; i++)
     {
-        pre[i] = pre[i - 1] + x[i];
+        m -= x[i];
+        ans = max(ans, i + m / mn * 2);
     }
-    long long l = 0, r = m;
-    while (l <= r)
-    {
-        long long mid = (l + r) / 2;
-        check(mid) ? l = mid + 1 : r = mid - 1;
-    }
-    cout << r << endl;
+    cout << ans << endl;
     return 0;
 }
