ca2931f49c702e6084de55330ebe1e2947f33f8e
Shanire <shanire@skylinejoy.com>
2025-12-04 15:34:55 +0800

    KMP

--- Luogu/P4391.cpp
+++ Luogu/P4391.cpp
@@ -1,23 +1,19 @@
 #include <algorithm>
 #include <iostream>
-#include <string>
 using namespace std;
 string s;
 int d[1000005], n;
 int main()
 {
-    cin >> n >> s;
-    for (int i = 1, j = 0; i < n; i++)
-    {
-        while (j > 0 && s[i] != s[j])
-        {
-            j = d[j - 1];
-        }
-        if (s[i] == s[j])
-        {
-            d[i] = ++j;
-        }
-    }
-    cout << n - d[n - 1] << "\n";
-    return 0;
+	cin >> n >> s;
+	for (int i = 1, j = 0; i < n; i++)
+	{
+		while (j > 0 && s[i] != s[j])
+		{
+			j = d[j - 1];
+		}
+		d[i] = (s[i] == s[j] ? ++j : 0);
+	}
+	cout << n - d[n - 1] << endl;
+	return 0;
 }
