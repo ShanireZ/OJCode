f773e27c62aa9a9dbd1b0be41a701e7ec61908db
Shanire <shanire@skylinejoy.com>
2025-12-04 11:35:49 +0800

    最小表示法

---
+++ Luogu/P13270.cpp
@@ -0,0 +1,32 @@
+#include <algorithm>
+#include <iostream>
+using namespace std;
+string s;
+int n, ex, p1, p2 = 1;
+int main()
+{
+	cin >> n >> s;
+	s += s;
+	while (ex < n && p1 < n && p2 < n)
+	{
+		if (s[p1 + ex] == s[p2 + ex])
+		{
+			ex++;
+		}
+		else
+		{
+			s[p1 + ex] > s[p2 + ex] ? p1 = p1 + ex + 1 : p2 = p2 + ex + 1;
+			ex = 0;
+			if (p1 == p2)
+			{
+				p2++;
+			}
+		}
+	}
+	for (int i = min(p1, p2), ex = 0; ex < n; ex++)
+	{
+		cout << s[i + ex];
+	}
+	cout << endl;
+	return 0;
+}
