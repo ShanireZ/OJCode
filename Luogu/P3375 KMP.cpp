8350608a40e24c989565e81b0de904acf631cd05
Shanire <shanire@skylinejoy.com>
2025-12-04 15:07:47 +0800

    KMP

--- Luogu/P3375 KMP.cpp
+++ Luogu/P3375 KMP.cpp
@@ -1,44 +1,36 @@
 #include <algorithm>
 #include <iostream>
-#include <string>
 using namespace std;
+int bod[1000005], n1, n2;
 string s1, s2;
-int d[1000005], sz1, sz2;
-void border()
-{
-    for (int i = 1, j = 0; i < sz2; i++)
-    {
-        while (j != 0 && s2[i] != s2[j])
-        {
-            j = d[j - 1];
-        }
-        d[i] = (s2[i] == s2[j] ? ++j : j);
-    }
-}
-void kmp()
-{
-    for (int i = 0, j = 0; i < sz1;)
-    {
-        while (i < sz1 && j < sz2 && s1[i] == s2[j])
-        {
-            i++, j++;
-        }
-        if (j == sz2)
-        {
-            cout << i - j + 1 << endl;
-        }
-        j == 0 ? i++ : j = d[j - 1];
-    }
-}
 int main()
 {
-    cin >> s1 >> s2;
-    sz1 = s1.size(), sz2 = s2.size();
-    border(), kmp();
-    for (int i = 0; i < sz2; i++)
-    {
-        cout << d[i] << " ";
-    }
-    return 0;
+	cin >> s1 >> s2;
+	n1 = s1.size(), n2 = s2.size();
+	for (int i = 1, j = 0; i < n2; i++)
+	{
+		while (j != 0 && s2[i] != s2[j])
+		{
+			j = bod[j - 1];
+		}
+		bod[i] = (s2[i] == s2[j] ? ++j : 0);
+	}
+	for (int i = 0, j = 0; i < n1; i++)
+	{
+		while (j != 0 && s1[i] != s2[j])
+		{
+			j = bod[j - 1];
+		}
+		j += (s1[i] == s2[j]);
+		if (j == n2)
+		{
+			cout << i - n2 + 2 << endl;
+		}
+	}
+	for (int i = 0; i < n2; i++)
+	{
+		cout << bod[i] << " ";
+	}
+	cout << endl;
+	return 0;
 }
-// TAG: KMP border AC自动机
