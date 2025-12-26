b2eaf04d7cfa251d9df3b1ae9e61c3990e117578
Shanire <shanire@skylinejoy.com>
2025-12-26 10:04:32 +0800

    Update

--- Luogu/P3796.cpp
+++ Luogu/P3796.cpp
@@ -1,117 +1,113 @@
 #include <algorithm>
-#include <cstring>
 #include <iostream>
 #include <queue>
-#include <string>
 using namespace std;
-#define MX 10505
-int trie[MX][30], step[MX], fail[MX], ind[MX], ed[155], pos;
-string s, all[155];
+int n, pos, root = 1, mx, fin[155], t[10505], d[10505], fail[10505], trie[10505][26];
+string s[155];
 queue<int> q;
-void maket(int x)
+void init(int i)
 {
-    int now = 1;
-    for (char c : s)
-    {
-        int id = c - 'a';
-        if (trie[now][id] == 0)
-        {
-            trie[now][id] = ++pos;
-        }
-        now = trie[now][id];
-    }
-    ed[x] = now;
+	int now = root;
+	for (char ch : s[i])
+	{
+		int x = ch - 'a';
+		if (trie[now][x] == 0)
+		{
+			trie[now][x] = ++pos;
+		}
+		now = trie[now][x];
+	}
+	fin[i] = now;
 }
-int findpos(int now, int id)
+int findpos(int now, int x)
 {
-    if (now != 0 && trie[now][id] == 0)
-    {
-        now = fail[now];
-    }
-    return now == 0 ? 1 : trie[now][id];
+	while (now != 0 && trie[now][x] == 0)
+	{
+		now = fail[now];
+	}
+	return now == 0 ? root : trie[now][x];
 }
-void makefail()
+void failt()
 {
-    q.push(1);
-    while (q.size())
-    {
-        int now = q.front();
-        q.pop();
-        for (int i = 0; i < 26; i++)
-        {
-            int nxt = trie[now][i], f = findpos(fail[now], i);
-            if (nxt == 0)
-            {
-                trie[now][i] = f;
-            }
-            else
-            {
-                fail[nxt] = f, q.push(nxt), ind[f]++;
-            }
-        }
-    }
-}
-void searchs()
-{
-    int now = 1;
-    for (char c : s)
-    {
-        int id = c - 'a';
-        now = findpos(now, id);
-        step[now]++;
-    }
+	q.push(root);
+	while (q.size())
+	{
+		int now = q.front();
+		q.pop();
+		for (int i = 0; i < 26; i++)
+		{
+			int nxt = trie[now][i];
+			int f = findpos(fail[now], i);
+			if (nxt == 0)
+			{
+				trie[now][i] = f;
+				continue;
+			}
+			fail[nxt] = f, d[f]++, q.push(nxt);
+		}
+	}
 }
 void tp()
 {
-    for (int i = 1; i <= pos; i++)
-    {
-        if (ind[i] == 0)
-        {
-            q.push(i);
-        }
-    }
-    while (q.size())
-    {
-        int now = q.front();
-        q.pop();
-        int nxt = fail[now];
-        ind[nxt]--, step[nxt] += step[now];
-        if (ind[nxt] == 0)
-        {
-            q.push(nxt);
-        }
-    }
+	for (int i = 1; i <= pos; i++)
+	{
+		if (d[i] == 0)
+		{
+			q.push(i);
+		}
+	}
+	while (q.size())
+	{
+		int now = q.front();
+		q.pop();
+		int nxt = fail[now];
+		t[nxt] += t[now], d[nxt]--;
+		if (d[nxt] == 0)
+		{
+			q.push(nxt);
+		}
+	}
 }
 int main()
 {
-    int n;
-    cin >> n;
-    while (n)
-    {
-        memset(trie, 0, sizeof(trie)), memset(fail, 0, sizeof(fail));
-        memset(step, 0, sizeof(step)), pos = 1;
-        for (int i = 1; i <= n; i++)
-        {
-            cin >> s;
-            all[i] = s, maket(i);
-        }
-        makefail();
-        cin >> s;
-        searchs(), tp();
-        int ans = 0;
-        for (int i = 1; i <= n; i++)
-        {
-            ans = max(ans, step[ed[i]]);
-        }
-        cout << ans << endl;
-        for (int i = 1; i <= n; i++)
-        {
-            if (ans == step[ed[i]])
-            {
-                cout << all[i] << endl;
-            }
-        }
-        cin >> n;
-    }
-    return 0;
+	cin >> n;
+	while (n)
+	{
+		pos = 1, mx = 0;
+		for (int i = 1; i <= n; i++)
+		{
+			cin >> s[i];
+			init(i);
+		}
+		failt();
+		cin >> s[0];
+		int now = root;
+		for (char ch : s[0])
+		{
+			int x = ch - 'a';
+			now = findpos(now, x);
+			t[now]++;
+		}
+		tp();
+		for (int i = 1; i <= n; i++)
+		{
+			mx = max(mx, t[fin[i]]);
+		}
+		cout << mx << endl;
+		for (int i = 1; i <= n; i++)
+		{
+			if (t[fin[i]] == mx)
+			{
+				cout << s[i] << endl;
+			}
+		}
+		cin >> n;
+		fill(fin, fin + 1 + n, 0);
+		for (int i = 1; i <= pos; i++)
+		{
+			fill(trie[i], trie[i] + 26, 0);
+			t[i] = d[i] = fail[i] = 0;
+		}
+	}
+	return 0;
 }
