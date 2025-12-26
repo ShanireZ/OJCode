3d6da0f71c16f09c791473907dbf68bda3147190
Shanire <shanire@skylinejoy.com>
2025-12-26 09:32:49 +0800

    Update

--- Luogu/P3808.cpp
+++ Luogu/P3808.cpp
@@ -1,82 +1,96 @@
 #include <algorithm>
 #include <iostream>
 #include <queue>
-#include <string>
 using namespace std;
 #define MX 1000005
-int trie[MX][30], vis[MX], fail[MX], ed[MX], pos = 1, ans;
+int trie[MX][26], t[MX], fin[MX], d[MX], fail[MX], n, ans, pos = 1, root = 1;
 string s;
 queue<int> q;
-void maket(int x)
+void init(int i)
 {
-    int now = 1;
-    for (char c : s)
-    {
-        int id = c - 'a';
-        if (trie[now][id] == 0)
-        {
-            trie[now][id] = ++pos;
-        }
-        now = trie[now][id];
-    }
-    ed[x] = now;
+	int now = root;
+	for (char ch : s)
+	{
+		int x = ch - 'a';
+		if (trie[now][x] == 0)
+		{
+			trie[now][x] = ++pos;
+		}
+		now = trie[now][x];
+	}
+	fin[i] = now;
 }
-int findpos(int now, int id)
+int findpos(int now, int x)
 {
-    if (now != 0 && trie[now][id] == 0)
-    {
-        now = fail[now];
-    }
-    return now == 0 ? 1 : trie[now][id];
+	while (now != 0 && trie[now][x] == 0)
+	{
+		now = fail[now];
+	}
+	return now == 0 ? root : trie[now][x];
 }
-void makefail()
+void failt()
 {
-    q.push(1);
-    while (q.size())
-    {
-        int now = q.front();
-        q.pop();
-        for (int i = 0; i < 26; i++)
-        {
-            int nxt = trie[now][i], f = findpos(fail[now], i);
-            if (nxt == 0)
-            {
-                trie[now][i] = f;
-            }
-            else
-            {
-                fail[nxt] = f, q.push(nxt);
-            }
-        }
-    }
+	q.push(root);
+	while (q.size())
+	{
+		int now = q.front();
+		q.pop();
+		for (int i = 0; i < 26; i++)
+		{
+			int nxt = trie[now][i];
+			int f = findpos(fail[now], i);
+			if (nxt == 0)
+			{
+				trie[now][i] = f;
+				continue;
+			}
+			fail[nxt] = f, d[f]++, q.push(nxt);
+		}
+	}
 }
-void searchs()
+void tp()
 {
-    int now = 1;
-    for (char c : s)
-    {
-        int id = c - 'a';
-        now = findpos(now, id);
-        vis[now] = 1;
-    }
+	for (int i = 1; i <= pos; i++)
+	{
+		if (d[i] == 0)
+		{
+			q.push(i);
+		}
+	}
+	while (q.size())
+	{
+		int now = q.front();
+		q.pop();
+		int nxt = fail[now];
+		t[nxt] += t[now], d[nxt]--;
+		if (d[nxt] == 0)
+		{
+			q.push(nxt);
+		}
+	}
 }
 int main()
 {
-    int n;
-    cin >> n;
-    for (int i = 1; i <= n; i++)
-    {
-        cin >> s;
-        maket(i);
-    }
-    makefail();
-    cin >> s;
-    searchs();
-    for (int i = 1; i <= n; i++)
-    {
-        ans += vis[ed[i]];
-    }
-    cout << ans << endl;
-    return 0;
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> s;
+		init(i);
+	}
+	failt();
+	cin >> s;
+	int now = root;
+	for (char ch : s)
+	{
+		int x = ch - 'a';
+		now = findpos(now, x);
+		t[now]++;
+	}
+	tp();
+	for (int i = 1; i <= n; i++)
+	{
+		ans += (t[fin[i]] != 0);
+	}
+	cout << ans << endl;
+	return 0;
 }
-// 可被课件样例hack
