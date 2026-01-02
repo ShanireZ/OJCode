4c95bf852f0c3a1e135b3f09f34f718a549f8ff0
ShanireZ <shanire@skylinejoy.com>
2026-01-02 15:58:31 +0800

    Update

--- Luogu/P14920.cpp
+++ Luogu/P14920.cpp
@@ -4,21 +4,21 @@ using namespace std;
 long long dp[250005], n, k, ans;
 int main()
 {
-	cin >> n >> k;
-	fill(dp + 1, dp + n * 500 + 1, 1e15);
-	for (int i = 1; i <= n; i++)
-	{
-		long long a, c;
-		cin >> a >> c;
-		for (int j = n * 500; j >= 1; j--)
-		{
-			dp[j] = min(dp[j], dp[j - a] + c);
-			if (dp[j] <= k)
-			{
-				ans = max(ans, 1ll * j);
-			}
-		}
-	}
-	cout << ans << endl;
-	return 0;
+    cin >> n >> k;
+    fill(dp + 1, dp + 1 + 250000, 1e18);
+    for (int i = 1; i <= n; i++)
+    {
+        long long a, c;
+        cin >> a >> c;
+        for (long long j = 250000; j >= a; j--)
+        {
+            dp[j] = min(dp[j], dp[j - a] + c);
+            if (dp[j] <= k)
+            {
+                ans = max(ans, j);
+            }
+        }
+    }
+    cout << ans << endl;
+    return 0;
 }
