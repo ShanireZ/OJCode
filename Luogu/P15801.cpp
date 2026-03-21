46aa3f220c80a1e7285c3077948beec8a142d675
Shanire <shanire@skylinejoy.com>
2026-03-21 11:31:59 +0800

    Update

--- Luogu/P15801.cpp
+++ Luogu/P15801.cpp
@@ -1,23 +1,29 @@
 #include <algorithm>
 #include <iostream>
 using namespace std;
-int n, ans, l[100005], r[100005], wq[100005], dp[100005], sz[100005];
+int l[100005], r[100005], wq[100005], m[100005], h[100005], n, ans;
 void dfs(int now)
 {
     if (l[now])
     {
-        dfs(l[now]), sz[now] += sz[l[now]];
+        dfs(l[now]);
     }
     if (r[now])
     {
-        dfs(r[now]), sz[now] += sz[r[now]];
+        dfs(r[now]);
     }
-    dp[now] = max(dp[l[now]], dp[r[now]]) + 1, sz[now] += 1;
+    h[now] = max(h[l[now]], h[r[now]]) + 1;
+    if (m[l[now]] && m[r[now]] && h[l[now]] == h[r[now]])
+    {
+        m[now] = 1;
+    }
     if (wq[l[now]] && wq[r[now]])
     {
-        int sl = (dp[l[now]] <= 20 && (1 << dp[l[now]]) == sz[l[now]] + 1);
-        int sr = (dp[r[now]] <= 20 && (1 << dp[r[now]]) == sz[r[now]] + 1);
-        if ((sl && dp[l[now]] == dp[r[now]]) || (sr && dp[r[now]] + 1 == dp[l[now]]))
+        if (m[l[now]] && h[l[now]] - h[r[now]] == 0)
+        {
+            wq[now] = 1;
+        }
+        else if (m[r[now]] && h[l[now]] - h[r[now]] == 1)
         {
             wq[now] = 1;
         }
@@ -31,7 +37,8 @@ int main()
     {
         cin >> l[i] >> r[i];
     }
-    wq[0] = 1, dfs(1);
+    wq[0] = m[0] = 1;
+    dfs(1);
     cout << ans << endl;
     return 0;
 }
